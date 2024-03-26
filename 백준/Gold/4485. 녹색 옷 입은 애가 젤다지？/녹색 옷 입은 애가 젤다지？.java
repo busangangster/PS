e6378d11b1619964939e3641a752c57169ec14c0
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-03-26 11:57:14 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 224 ms, Memory: 18892 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -2,63 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N,INF;
-	static int[][] arr;
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {1,0,-1,0};
-	static int cnt;
-	static int[][] min_dis;
+	static int n,cnt,INF;
+	static int[][] graph,min_dis;
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,1,0,-1};
 	static StringBuilder sb = new StringBuilder();
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		INF = Integer.MAX_VALUE;
 		cnt = 1;
+		INF = Integer.MAX_VALUE;
 		
 		while (true) {
-			N = Integer.parseInt(br.readLine());
-			if (N == 0) break;
+			n = Integer.parseInt(br.readLine());
+			if (n == 0) break;
 			else {
+				graph = new int[n][n];
 				
-				arr = new int[N][N];
-
-				for (int i=0; i<N; i++) {
+				for (int i=0; i<n; i++) {
 					st = new StringTokenizer(br.readLine());
-					for (int j=0; j<N; j++) {
-						arr[i][j] = Integer.parseInt(st.nextToken());
+					for (int j=0; j<n; j++) {
+						graph[i][j] = Integer.parseInt(st.nextToken());
 					}
 				}
-				dijkstra();
+//				System.out.println(Arrays.deepToString(graph));
+				dijkstra(cnt);
+
 			}
 			cnt++;
+			
 		}
 		System.out.println(sb);
+		
 	}
 	
-	public static void dijkstra() {
-		PriorityQueue<Node> pq = new PriorityQueue<Node>((o1,o2) -> o1.rupee - o2.rupee);
-		
-		min_dis = new int[N][N];
-		
-		for (int i=0; i<N; i++) {
-			for (int j=0; j<N; j++) {
-				min_dis[i][j] = INF;
-			}
+	public static void dijkstra(int cnt) {
+		PriorityQueue<Node> pq = new PriorityQueue<>(); 
+		min_dis = new int[n][n];
+		for (int[] next: min_dis) {
+			Arrays.fill(next, INF);
 		}
-		
-		min_dis[0][0] = arr[0][0];
-		
-		pq.offer(new Node(0,0,arr[0][0]));
+		pq.offer(new Node(0,0,graph[0][0]));
 		
 		while (!pq.isEmpty()) {
-			
 			Node cur = pq.poll();
-
-			if (cur.x == N-1 && cur.y == N-1) {
-				sb.append("Problem").append(" ").append(cnt).append(":").append(" ").append(min_dis[N-1][N-1]);
-				sb.append("\n");
+			
+			if (cur.x == n-1 && cur.y == n-1) {
+				sb.append("Problem").append(" ").append(cnt).append(": ").append(cur.c).append("\n");
 			}
 			
 			for (int i=0; i<4; i++) {
@@ -66,33 +57,33 @@ public class Main {
 				int ny = cur.y + dy[i];
 				
 				if (check(nx,ny)) {
-					int cost = cur.rupee + arr[nx][ny];
+					int cost = cur.c + graph[nx][ny];
 					if (min_dis[nx][ny] > cost) {
 						min_dis[nx][ny] = cost;
-						pq.offer(new Node(nx,ny,cost));	
+						pq.offer(new Node(nx,ny,cost));
 					}
 				}
 			}
+			
 		}
 	}
-
+	
 	public static boolean check(int x, int y) {
-		if (0<= x &&  x < N && 0 <= y && y < N) {
-			return true;
-		}
-		return false;
+		if (0 <= x && x < n && 0 <= y && y < n) return true;
+		else return false;
 	}
-
+	
 }
 
-class Node{
-	int x;
-	int y;
-	int rupee;
-	
-	public Node(int x, int y, int rupee) {
+class Node implements Comparable<Node>{
+	int x,y,c;
+	public Node(int x, int y, int c) {
 		this.x = x;
-		this.y = y;
-		this.rupee = rupee;
+		this.y = y ;
+		this.c = c;
+	}
+	@Override
+	public int compareTo(Node o) {
+		return this.c - o.c;
 	}
 }

114619c887e083d52c20674caac675760dae387d
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-03-27 12:12:38 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 588 ms, Memory: 121632 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	static int[] arr,lis;
+	static int N,len;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        
+        arr = new int[N];
+        lis = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        len = 1;
+        lis[0] = arr[0];
+        for (int i=1; i<N; i++) {
+        	if (lis[len-1] < arr[i]) {
+        		lis[len++] = arr[i];
+        	}
+        	else if (lis[len-1] > arr[i]) {
+        		int tmp = bs(arr[i]);
+        		lis[tmp] = arr[i];
+        	}
+        }
+        System.out.println(len);
+    }
+    
+    public static int bs(int target) {
+    	int lt = 0;
+    	int rt = len-1;
+    	
+    	while (lt < rt) {
+    		int mid = (lt+rt) / 2;
+    		
+    		if (lis[mid] < target) {
+    			lt = mid+1;
+    		}
+    		else {
+    			rt = mid;
+    		}
+    	}
+    	return rt;
+    }
+}

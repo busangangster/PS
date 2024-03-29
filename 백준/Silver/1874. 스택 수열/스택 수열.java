9124ce2418e11795390fab5bff677b8d71b7c785
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-03-29 11:05:35 +0900

    [Silver II] Title: 스택 수열, Time: 360 ms, Memory: 27792 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,33 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Stack<Integer> stack = new Stack<Integer>();
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		int cnt = 1;
-		for (int i=0;i<n;i++) {
-			int num = Integer.parseInt(br.readLine());
-			
-			while (cnt <= num) {
-				stack.push(cnt);
-				cnt++;
-				sb.append('+').append("\n");
-			}
-			if (stack.peek() == num) {
-				stack.pop();
-				sb.append('-').append("\n");
-			}
-			
-		}
-		if (stack.isEmpty()) {
-			System.out.print(sb);
-		} else {
-			System.out.print("NO");
-		}
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        int cnt = 1;
+        for (int i=0; i<n; i++) {
+        	int num = Integer.parseInt(br.readLine());
+        	
+        	while (cnt <= num) {
+        		stack.push(cnt);
+        		cnt++;
+        		sb.append("+").append("\n");
+        	}
+    		
+    		if (stack.peek() == num) {
+    			stack.pop();
+    			sb.append("-").append("\n");
+    		}	
+        }
+
+        if (!stack.isEmpty()) {
+        	System.out.println("NO");
+        }
+        else {
+        	System.out.println(sb);	
+        }
+        
 	}
-
 }

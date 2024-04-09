c58c2eb49750ddb38b7f8f1397dbd6cc35117166
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-04-09 09:32:37 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 122 ms, Memory: 26,288 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -1,94 +1,64 @@
-import java.io.*;
 import java.util.*;
- 
-class Solution {
-	static int N,min_v,max_v;
-	static int[] arr,operators,op_case;
-	static boolean[] visited;
+import java.io.*;
 
+public class Solution {
+	static int N,min_v,max_v;
+	static int[] op;
+	static int[] numbers;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
+
+		int t = Integer.parseInt(br.readLine());
 		
-		int T = Integer.parseInt(br.readLine());
-
-		for (int tc=1; tc<=T; tc++) {
+		for (int i=1; i<=t; i++) {
 			N = Integer.parseInt(br.readLine());
-
-			arr = new int[N];
-			visited = new boolean[N-1];
-			operators = new int[4];
-			op_case = new int[N-1];
+			
+			op = new int[4];
+			numbers = new int[N];
 			min_v = Integer.MAX_VALUE;
 			max_v = Integer.MIN_VALUE;
-
+			
 			st = new StringTokenizer(br.readLine());
-			for (int i=0; i<4; i++) {
-				operators[i] = Integer.parseInt(st.nextToken());
-			}
-
-			st = new StringTokenizer(br.readLine());
-			for (int i=0; i<N; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
+			for (int j=0; j<4; j++) {
+				op[j] = Integer.parseInt(st.nextToken());
 			}
 			
-			perm(0);
-			sb.append("#").append(tc).append(" ").append(max_v - min_v).append("\n");
+			st = new StringTokenizer(br.readLine());
+			for (int j=0; j<N; j++) {
+				numbers[j] = Integer.parseInt(st.nextToken());
+			}
+			
+			dfs(1,numbers[0],op[0], op[1],op[2],op[3]);
+			sb.append("#").append(i).append(" ").append(max_v - min_v).append("\n");
+			
 		}
 		System.out.println(sb);
-
 	}
-
-	static void perm(int cnt) {
-		if (cnt == N-1) {
-			cal();
+	
+	static void dfs(int depth, int total, int plus, int minus, int mutiply, int divide) {
+		if (depth == N) {
+			min_v = Math.min(min_v, total);
+			max_v = Math.max(max_v, total);
 			return;
 		}
-
-		for (int i=0; i<4; i++) {
-
-			if (operators[i] ==  0) continue;
-
-			op_case[cnt] = i;
-			operators[i]--;
-			perm(cnt+1);
-			operators[i]++;
-
-		}
-	}
-
-	static void cal() {
-		int res = arr[0];
-		int cnt = 1;
 		
-
-		for (int i=0; i<N-1;i++) {
-			switch (op_case[i]) {
-				case 0:
-					res += arr[cnt];
-					cnt++;
-					break;
-				case 1:
-					res -= arr[cnt];
-					cnt++;
-					break;
-	
-				case 2:
-					res *= arr[cnt];
-					cnt++;
-					break;
-	
-				case 3:
-					res /= arr[cnt];
-					cnt++;
-					break;
-	
-			}
+		if (plus != 0) {
+			dfs(depth+1,total+ numbers[depth],plus-1,minus,mutiply,divide);
+			
 		}
+		if (minus != 0) {
+			dfs(depth+1,total - numbers[depth],plus,minus-1,mutiply,divide);
 
-		min_v = Math.min(min_v,res);
-		max_v = Math.max(max_v,res);
+		}
+		if (mutiply != 0) {
+			dfs(depth+1,total * numbers[depth],plus,minus,mutiply-1,divide);
 
+		}
+		if (divide != 0) {
+			dfs(depth+1,total / numbers[depth],plus,minus,mutiply,divide-1);
+			
+		}
 	}
 }

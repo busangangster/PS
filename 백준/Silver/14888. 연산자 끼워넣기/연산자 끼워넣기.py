01d085f42ef9e95f179db6b6e28b49684be28ee3
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-04-09 09:23:50 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+max_v = -2147000000
+min_v = 2147000000
+
+def DFS(depth,total,plus,minus,multiply,divide):
+  global max_v,min_v
+  if depth == n:
+    max_v = max(max_v,total)
+    min_v = min(min_v,total)
+  else:
+    if plus:
+      DFS(depth+1,total+a[depth],plus-1,minus,multiply,divide)
+    if minus:
+      DFS(depth+1,total-a[depth],plus,minus-1,multiply,divide)
+    if multiply:
+      DFS(depth+1,total*a[depth],plus,minus,multiply-1,divide)
+    if divide:
+      DFS(depth+1,int(total/a[depth]),plus,minus,multiply,divide-1)
+
+DFS(1,a[0],b[0],b[1],b[2],b[3])
+print(max_v)
+print(min_v)

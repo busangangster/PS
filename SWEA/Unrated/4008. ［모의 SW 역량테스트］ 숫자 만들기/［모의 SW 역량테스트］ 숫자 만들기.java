d4dfd894e0515dbb2c23f3307f8b5113069a11a3
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-04-11 10:44:36 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 126 ms, Memory: 30,024 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -3,62 +3,67 @@ import java.io.*;
 
 public class Solution {
 	static int N,min_v,max_v;
-	static int[] op;
-	static int[] numbers;
+	static int[] op,nums;
+	static boolean[] isSelected;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-
+		
 		int t = Integer.parseInt(br.readLine());
 		
-		for (int i=1; i<=t; i++) {
+		for (int tc=1; tc<=t; tc++) {
 			N = Integer.parseInt(br.readLine());
 			
+			
 			op = new int[4];
-			numbers = new int[N];
+			nums = new int[N];
+			isSelected = new boolean[N];
 			min_v = Integer.MAX_VALUE;
 			max_v = Integer.MIN_VALUE;
 			
 			st = new StringTokenizer(br.readLine());
-			for (int j=0; j<4; j++) {
-				op[j] = Integer.parseInt(st.nextToken());
+			for (int i=0; i<4; i++) {
+				op[i] = Integer.parseInt(st.nextToken());
 			}
 			
 			st = new StringTokenizer(br.readLine());
-			for (int j=0; j<N; j++) {
-				numbers[j] = Integer.parseInt(st.nextToken());
+			for (int i=0; i<N; i++) {
+				nums[i] = Integer.parseInt(st.nextToken());
 			}
+			dfs(nums[0],1,op[0],op[1],op[2],op[3]);
 			
-			dfs(1,numbers[0],op[0], op[1],op[2],op[3]);
-			sb.append("#").append(i).append(" ").append(max_v - min_v).append("\n");
-			
+			sb.append("#").append(tc).append(" ").append(max_v-min_v).append("\n");
 		}
 		System.out.println(sb);
+		
+
 	}
 	
-	static void dfs(int depth, int total, int plus, int minus, int mutiply, int divide) {
-		if (depth == N) {
+	static void dfs(int total, int idx, int plus, int minus, int multiply, int divide) {
+		if (idx == N) {
+			
 			min_v = Math.min(min_v, total);
 			max_v = Math.max(max_v, total);
+			
 			return;
 		}
 		
 		if (plus != 0) {
-			dfs(depth+1,total+ numbers[depth],plus-1,minus,mutiply,divide);
-			
+			dfs(total+nums[idx],idx+1,plus-1,minus,multiply,divide);
 		}
+		
 		if (minus != 0) {
-			dfs(depth+1,total - numbers[depth],plus,minus-1,mutiply,divide);
-
+			dfs(total-nums[idx],idx+1,plus,minus-1,multiply,divide);
 		}
-		if (mutiply != 0) {
-			dfs(depth+1,total * numbers[depth],plus,minus,mutiply-1,divide);
-
+		
+		if (multiply != 0) {
+			dfs(total*nums[idx],idx+1,plus,minus,multiply-1,divide);
 		}
-		if (divide != 0) {
-			dfs(depth+1,total / numbers[depth],plus,minus,mutiply,divide-1);
-			
+		
+		if (divide != 0 ) {
+			dfs(total/nums[idx],idx+1,plus,minus,multiply,divide-1);
 		}
+		
 	}
 }

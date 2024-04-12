044e1e79ea92734c29e084f33820566d74ae1678
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-04-12 10:43:26 +0900

    랜선 자르기.java

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,8 +1,15 @@
 import java.io.*;
 import java.util.*;
 
+/*
+CheckPoints !!
+
+- 이분탐색문제인 건 빠르게 알았지만, 자료형을 잘 체크했어야 함.
+lt가 1이고, rt는 최댓값인 2^31-1이 들어가는데, 이 둘을 더할 경우 int의 범위를 벗어나게 됨.
+떄문에 long형으로 선언해줬어야 함 !! 
+*/
+
 public class Main {
-	
 	static int K,N,max_v;
 	static int[] arr;
 	public static void main(String[] args) throws Exception{

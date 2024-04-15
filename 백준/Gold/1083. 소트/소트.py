7782c9b6bb4dc511e1c581a837ba567ca53d6705
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-04-15 16:38:21 +0900

    [Gold V] Title: 소트, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1083. 소트/소트.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+s = int(input())
+
+for i in range(n):
+  mx = max(a[i:min(n, i+s+1)])
+  idx = a.index(mx)
+
+  for j in range(idx,i,-1):
+    a[j],a[j-1] = a[j-1],a[j]
+
+  s -= (idx - i)
+  if s <= 0:
+    break
+  
+print(*a)

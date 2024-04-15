8c7f7fd28f953d71fbe3c861f29783f96cec7855
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-04-15 11:55:02 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 980 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		int D = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int A = 0;
+		int B = 0;
+
+		int[] dp = new int[31];
+
+		outer: for (int i = 1; i <= 50000; i++) {
+			dp[1] = i;
+			for (int j = i; j <= 50000; j++) {
+				dp[2] = j;
+				for (int z = 3; z <= 30; z++) {
+					dp[z] = dp[z - 1] + dp[z - 2];
+					if (z == D) {
+						if (dp[z] != K) {
+							break;
+						} else if (dp[z] == K) {
+							A = i;
+							B = j;
+							break outer;
+						}
+					}
+
+				}
+			}
+		}
+
+		System.out.println(A);
+		System.out.println(B);
+
+	}
+}

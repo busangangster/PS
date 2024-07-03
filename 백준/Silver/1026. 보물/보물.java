4e4533031111ad6946b78dee5bbcca4cebe51f05
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-07-03 16:18:50 +0900

    [Silver IV] Title: 보물, Time: 104 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] a = new int[n];
+        Integer[] b = new Integer[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b, Collections.reverseOrder());
+        
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (a[i] * b[i]);
+        }
+        System.out.println(ans);
+    }
+}

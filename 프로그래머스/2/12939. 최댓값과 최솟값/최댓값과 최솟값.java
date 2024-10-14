50b8be97ad65543910a971fc595de834ddf6f3fd
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-10-14 16:59:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 14.34 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,14 +1,15 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public String solution(String s) {
-        String answer = "";
+        String answer = " ";
         String[] arr = s.split(" ");
-        int[] arr2 = new int[arr.length];
-        for (int i = 0; i < arr2.length; i++) arr2[i] = Integer.parseInt(arr[i]);
-        Arrays.sort(arr2);
-        
-        return arr2[0] + " " + arr2[arr2.length - 1];
+        ArrayList<Integer> arr2 = new ArrayList<Integer>();
+        for (String val : arr) {
+            arr2.add(Integer.parseInt(val));
+        }
+        Collections.sort(arr2);
+        // System.out.println(arr2.toString());
+        return arr2.get(0) + " " + arr2.get(arr2.size() -1 );
     }
 }

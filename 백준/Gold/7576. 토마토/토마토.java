b0d920e0bd093f47895b35ee740a1bf324e8f270
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-06-09 13:57:43 +0900

    [Gold V] Title: 토마토, Time: 540 ms, Memory: 103828 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,94 +1,99 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	static int N, M;
-	static int[][] graph;
-	static ArrayList<Node> tomato;
-	static int[] dx = { 1, 0, -1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
+public class Main {
+  public static int N, M, day;
+  public static int[][] arr;
+  public static boolean[][] visited;
+  public static Queue<Node> q = new ArrayDeque<Node>();
+  public static int[] dx = { 0, 1, 0, -1 };
+  public static int[] dy = { 1, 0, -1, 0 };
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
+    st = new StringTokenizer(br.readLine());
+    M = Integer.parseInt(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
+    day = 0;
 
-		graph = new int[N][M];
-		tomato = new ArrayList<>();
+    arr = new int[N][M];
+    visited = new boolean[N][M];
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				graph[i][j] = Integer.parseInt(st.nextToken());
-				if (graph[i][j] == 1)
-					tomato.add(new Node(i, j, 0));
-			}
-		}
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < M; j++) {
+        int tmp = Integer.parseInt(st.nextToken());
+        if (tmp == 1) {
+          q.offer(new Node(i, j, 0));
+          visited[i][j] = true;
+        } else if (tmp == -1) {
+          visited[i][j] = true;
+        }
+        arr[i][j] = tmp;
+      }
+    }
 
-		int ans = bfs();
-		if (!checkTomato()) {
-			System.out.println(-1);
-		} else {
-			System.out.println(ans);
-		}
+    if (verify()) {
+      System.out.println(0);
+    } else {
+      bfs();
+      if (verify()) {
+        System.out.println(day);
+      } else {
+        System.out.println(-1);
+      }
+    }
+  }
 
-	}
+  public static void bfs() {
 
-	static int bfs() {
-		Queue<Node> q = new ArrayDeque<>();
-		int tmp = 0;
-		for (Node val : tomato) {
-			q.offer(new Node(val.x, val.y, val.time));
-		}
+    while (!q.isEmpty()) {
+      Node cur = q.poll();
 
-		while (!q.isEmpty()) {
-			Node cur = q.poll();
-			tmp = cur.time;
+      day = cur.cnt;
 
-			for (int i = 0; i < 4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
+      for (int i = 0; i < 4; i++) {
+        int nx = cur.x + dx[i];
+        int ny = cur.y + dy[i];
 
-				if (!check(nx, ny))
-					continue;
-				if (graph[nx][ny] != 0)
-					continue;
+        if (check(nx, ny)) {
+          if (!visited[nx][ny]) {
+            if (arr[nx][ny] == 0) {
+              visited[nx][ny] = true;
+              q.offer(new Node(nx, ny, cur.cnt + 1));
+            }
+          }
+        }
+      }
+    }
+  }
 
-				graph[nx][ny] = cur.time + 1;
-				q.offer(new Node(nx, ny, cur.time + 1));
-			}
-		}
-		return tmp;
-	}
+  public static boolean check(int x, int y) {
+    return (0 <= x && x < N && 0 <= y && y < M);
+  }
 
-	static boolean checkTomato() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (graph[i][j] == 0) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+  public static boolean verify() {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        if (!visited[i][j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
 
-	static boolean check(int x, int y) {
-		if (0 <= x && x < N && 0 <= y && y < M)
-			return true;
-		else
-			return false;
-	}
+class Node {
+  int x;
+  int y;
+  int cnt;
 
-	static class Node {
-		int x, y, time;
-
-		public Node(int x, int y, int time) {
-			this.x = x;
-			this.y = y;
-			this.time = time;
-		}
-	}
+  public Node(int x, int y, int cnt) {
+    this.x = x;
+    this.y = y;
+    this.cnt = cnt;
+  }
 }

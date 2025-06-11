f231f474f00dd0a180a335fcfda120d513c57f4a
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-06-11 11:49:17 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1032 ms, Memory: 176608 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,34 +1,32 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main { 
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args)  throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		HashMap<Integer,Integer> hm = new HashMap<>();
-		int N = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
+    HashMap<Integer, Integer> hm = new HashMap<>();
 
-		for (int i=0; i<N; i++) {
-			int k = Integer.parseInt(st.nextToken());
-			hm.put(k,hm.getOrDefault(k, 0)+1);
-		}
+    int N = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      int tmp = Integer.parseInt(st.nextToken());
+      hm.put(tmp, hm.getOrDefault(tmp, 0) + 1);
+    }
 
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<M; i++) {
-			int tmp = Integer.parseInt(st.nextToken());
-			if (hm.containsKey(tmp)){
-				sb.append(hm.get(tmp));
-			}
-			else {
-				sb.append(0);
-			}
-			sb.append(" ");
-		}
-		System.out.println(sb);
-	}
+    int m = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < m; i++) {
+      int tmp = Integer.parseInt(st.nextToken());
+      if (hm.containsKey(tmp))
+        sb.append(hm.get(tmp));
+      else
+        sb.append(0);
+      sb.append(" ");
+    }
+
+    System.out.println(sb);
+  }
 }

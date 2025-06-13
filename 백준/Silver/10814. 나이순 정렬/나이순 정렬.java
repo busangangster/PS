daef57480fbda2d4e0c1b23e1f88c7952bc41393
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-06-13 09:37:53 +0900

    [Silver V] Title: 나이순 정렬, Time: 656 ms, Memory: 52480 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,46 +2,48 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		ArrayList<Member> arr = new ArrayList<Member>();
-		
-		for (int i=1; i<=n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int age = Integer.parseInt(st.nextToken());
-			String name = st.nextToken();
-			int num = i;
-			arr.add(new Member(age,name,num));
-		}
-		Collections.sort(arr);
-		
-		for (Member x: arr) {
-			sb.append(x.age).append(" ").append(x.name).append("\n");
-		}
-		System.out.println(sb);
-		
-	}
-	
-	static class Member implements Comparable<Member>{
-		int age, num;
-		String name;
-		
-		public Member(int age, String name, int num) {
-			this.age = age;
-			this.name = name;
-			this.num = num;
-		}
-		@Override
-		public int compareTo(Member o) {
-			if ( this.age == o.age) { 
-				return this.num - o.num;
-			}
-			return this.age - o.age;		
-		}
-	}
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+    int n = Integer.parseInt(br.readLine());
+
+    ArrayList<Person> ppl = new ArrayList<>();
+
+    for (int i = 1; i <= n; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      String name = st.nextToken();
+      ppl.add(new Person(a, name, i));
+    }
+
+    Collections.sort(ppl);
+    for (Person p : ppl) {
+      sb.append(p.age + " " + p.name).append("\n");
+    }
+
+    System.out.println(sb);
+
+  }
+}
+
+class Person implements Comparable<Person> {
+  int age;
+  String name;
+  int num;
+
+  public Person(int age, String name, int num) {
+    this.age = age;
+    this.name = name;
+    this.num = num;
+  }
+
+  @Override
+  public int compareTo(Person o) {
+    if (this.age == o.age) {
+      return this.num - o.num;
+    }
+    return this.age - o.age;
+  }
 }

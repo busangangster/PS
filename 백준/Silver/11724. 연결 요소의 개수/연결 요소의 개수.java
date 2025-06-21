f25b8e01ae493c14bbd4e359aa5c69b04fb92c73
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-06-21 09:38:34 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 560 ms, Memory: 142012 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,57 +1,56 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-	static int N,M;
-	static ArrayList<ArrayList<Integer>> arr;
-	static boolean[] visited;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-			
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new ArrayList<ArrayList<Integer>>();
-		visited = new boolean[N+1];
-		int cnt = 0;
-		
-		for (int i=0; i<=N; i++) {
-			arr.add(new ArrayList<>());
-		}
-		
-		for (int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			arr.get(u).add(v);
-			arr.get(v).add(u);
-		}
-		
-		for (int i=1; i<=N; i++) {
-			if (!visited[i]) {
-				bfs(i);
-				cnt++;
-			}
-		}
-		System.out.println(cnt);
+import java.io.*;
 
-	}
-	
-	static void bfs(int start) {
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		visited[start] = true;
-		q.offer(start);
-		
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-			
-			for (int next: arr.get(cur)) {
-				if (!visited[next]) {
-					visited[next] = true;
-					q.offer(next);
-				}
-			}
-		}
-	}
+public class Main {
+  public static int N, M;
+  public static boolean[] visited;
+  public static ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    visited = new boolean[N + 1];
+    for (int i = 0; i <= N; i++) {
+      arr.add(new ArrayList<>());
+    }
+
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      arr.get(a).add(b);
+      arr.get(b).add(a);
+    }
+
+    int cnt = 0;
+    for (int i = 1; i <= N; i++) {
+      if (!visited[i]) {
+        bfs(i);
+        cnt++;
+      }
+    }
+    System.out.println(cnt);
+  }
+
+  public static void bfs(int start) {
+    Queue<Integer> q = new ArrayDeque<>();
+    visited[start] = true;
+    q.offer(start);
+
+    while (!q.isEmpty()) {
+      int cur = q.poll();
+
+      for (int x : arr.get(cur)) {
+        if (!visited[x]) {
+          visited[x] = true;
+          q.offer(x);
+        }
+      }
+    }
+  }
 }

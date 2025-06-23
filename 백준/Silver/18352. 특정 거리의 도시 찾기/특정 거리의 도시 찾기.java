99e3ff09a0676f60c2a86d4613a2ea4e14a47904
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-06-23 09:30:49 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1344 ms, Memory: 336288 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,75 +1,75 @@
-import java.io.*;
 import java.util.*;
- 
-class Main {
-	static int N,M,K,X,INF;
-	static ArrayList<ArrayList<Node>> arr;
-	static int[] min_dis;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine());
+import java.io.*;
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		X = Integer.parseInt(st.nextToken());
+public class Main {
+  public static int N, M, K, X, INF;
+  public static int[] min_dis;
+  public static ArrayList<ArrayList<Node>> arr = new ArrayList<ArrayList<Node>>();
 
-		arr = new ArrayList<ArrayList<Node>>();
-		INF = 1000001;
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
 
-		for (int i=0; i<=N; i++) {
-			arr.add(new ArrayList<>());
-		}
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+    X = Integer.parseInt(st.nextToken());
 
-		for (int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			arr.get(a).add(new Node(b,1));
-		}
+    for (int i = 0; i <= N; i++) {
+      arr.add(new ArrayList<>());
+    }
+    INF = 1000000 + 1;
+    min_dis = new int[N + 1];
+    Arrays.fill(min_dis, INF);
 
-		int[] tmp = dijkstra(X);
-		for (int i=0; i<=N; i++) {
-			if (tmp[i] == K) {
-				sb.append(i).append("\n");
-			}
-		}
-		System.out.println(sb.length() == 0 ? -1 : sb);
-	}
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      arr.get(a).add(new Node(b, 1));
+    }
 
-	static int[] dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		min_dis = new int[N+1];
-		Arrays.fill(min_dis,INF);
-		min_dis[start] = 0;
-		pq.offer(new Node(start,0));
+    dijkstra();
 
-		while(!pq.isEmpty()){
-			Node cur = pq.poll();
+    for (int i = 1; i <= N; i++) {
+      if (min_dis[i] == K) {
+        sb.append(i).append("\n");
+      }
+    }
 
-			if (min_dis[cur.node] < cur.cost) continue;
+    System.out.println(sb.length() == 0 ? -1 : sb);
 
-			for (Node next: arr.get(cur.node)){
-				if (min_dis[next.node] > cur.cost + next.cost) {
-					min_dis[next.node] = cur.cost + next.cost;
-					pq.offer(new Node(next.node, cur.cost + next.cost));
-				}
-			}
-		}
-		return min_dis;
-	}
-	static class Node implements Comparable<Node>{
-		int node,cost;
-		public Node(int node, int cost) {
-			this.node = node;
-			this.cost = cost;
-		}
-		@Override
-		public int compareTo(Node o) {
-			return this.cost - o.cost;
-		}
-	}
+  }
+
+  public static void dijkstra() {
+    PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+    min_dis[X] = 0;
+    pq.offer(new Node(X, 0));
+
+    while (!pq.isEmpty()) {
+      Node cur = pq.poll();
+
+      if (min_dis[cur.node] < cur.cost)
+        continue;
+
+      for (Node next : arr.get(cur.node)) {
+        if (min_dis[next.node] > cur.cost + next.cost) {
+          min_dis[next.node] = cur.cost + next.cost;
+          pq.offer(new Node(next.node, cur.cost + next.cost));
+        }
+      }
+    }
+  }
+}
+
+class Node {
+  int node;
+  int cost;
+
+  public Node(int node, int cost) {
+    this.node = node;
+    this.cost = cost;
+  }
 }

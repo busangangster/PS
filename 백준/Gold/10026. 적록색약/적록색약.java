eba70976703fca4e2aaf04a33909eb76bdc66da5
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-06-24 09:37:18 +0900

    [Gold V] Title: 적록색약, Time: 144 ms, Memory: 16520 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,88 +1,120 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main {
-	static int N;
-	static char[][] graph,copy;
-	static boolean[][] visited1,visited2;
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		N = Integer.parseInt(br.readLine());
-		
-		graph = new char[N][N];
-		copy = new char[N][N];
-		
-		for (int i=0; i<N; i++) {
-			String s = br.readLine();
-			for (int j=0; j<N; j++) {
-				graph[i][j] = s.charAt(j);
-				if (graph[i][j] == 'R') {
-					copy[i][j] = 'G';
-				}
-				else {
-					copy[i][j] = graph[i][j];
-				}
-			}
-		}
-				
-		visited1 = new boolean[N][N];
-		visited2 = new boolean[N][N];
-		int cnt1 = 0;
-		int cnt2 = 0;
-		for (int i=0;i <N; i++) {
-			for (int j=0; j<N; j++) {
-				if (!visited1[i][j]) {
-					bfs(i,j,graph,visited1);
-					cnt1++;
-				}
-				if (!visited2[i][j]) {
-					bfs(i,j,copy,visited2);
-					cnt2++;
-				}
-			}
-		}
-		System.out.println(cnt1 + " " + cnt2);
-	}
-	
-	static void bfs(int x, int y, char[][] arr, boolean[][] visit) {
-		Queue<Node> q = new ArrayDeque<>();
-		visit[x][y] = true;
-		q.offer(new Node(x,y));
-		
-		while (!q.isEmpty()) {
-			Node cur = q.poll();
-			
-			for (int i=0; i<4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				
-				if (!check(nx,ny)) continue;
-				if (visit[nx][ny]) continue;
-				
-				if (arr[cur.x][cur.y]== arr[nx][ny]) {
-					q.offer(new Node(nx,ny));
-					visit[nx][ny] = true;
-				}
-			}
-		}
-		
-	}
-	
-	static boolean check(int x, int y) {
-		if (0 <= x && x < N && 0 <= y && y < N) return true;
-		else return false;
-	}
-	
-	static class Node{
-		int x,y;
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-    
+  public static int N;
+  public static char[][] arr;
+  public static boolean[][] visited;
+  public static int[] dx = { 0, 1, 0, -1 };
+  public static int[] dy = { 1, 0, -1, 0 };
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    N = Integer.parseInt(br.readLine());
+
+    arr = new char[N][N];
+    for (int i = 0; i < N; i++) {
+      String s = br.readLine();
+      for (int j = 0; j < N; j++) {
+        arr[i][j] = s.charAt(j);
+      }
+    }
+
+    int first = 0;
+    visited = new boolean[N][N];
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (!visited[i][j]) {
+          bfs1(i, j);
+          first++;
+        }
+      }
+    }
+
+    int second = 0;
+    visited = new boolean[N][N];
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (!visited[i][j]) {
+          bfs2(i, j);
+          second++;
+        }
+      }
+    }
+
+    System.out.println(second + " " + first);
+
+  }
+
+  public static void bfs1(int x, int y) {
+    Queue<Node> q = new ArrayDeque<>();
+    visited[x][y] = true;
+    q.offer(new Node(x, y));
+
+    while (!q.isEmpty()) {
+      Node cur = q.poll();
+
+      for (int i = 0; i < 4; i++) {
+        int nx = cur.x + dx[i];
+        int ny = cur.y + dy[i];
+
+        if (check(nx, ny)) {
+          if (!visited[nx][ny]) {
+            if (arr[cur.x][cur.y] != 'B') {
+              if (arr[nx][ny] == 'R' || arr[nx][ny] == 'G') {
+                visited[nx][ny] = true;
+                q.offer(new Node(nx, ny));
+              }
+            } else {
+              if (arr[nx][ny] == 'B') {
+                visited[nx][ny] = true;
+                q.offer(new Node(nx, ny));
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  public static void bfs2(int x, int y) {
+    Queue<Node> q = new ArrayDeque<>();
+    visited[x][y] = true;
+    q.offer(new Node(x, y));
+
+    while (!q.isEmpty()) {
+      Node cur = q.poll();
+
+      for (int i = 0; i < 4; i++) {
+        int nx = cur.x + dx[i];
+        int ny = cur.y + dy[i];
+
+        if (check(nx, ny)) {
+          if (!visited[nx][ny]) {
+            if (arr[cur.x][cur.y] == arr[nx][ny]) {
+              q.offer(new Node(nx, ny));
+              visited[nx][ny] = true;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  public static boolean check(int x, int y) {
+    return (0 <= x && x < N && 0 <= y && y < N);
+  }
+
+}
+
+class Node {
+  int x;
+  int y;
+
+  public Node(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
 }

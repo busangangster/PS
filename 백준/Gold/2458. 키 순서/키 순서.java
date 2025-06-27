40c9ef4cb7ef0bdf7d29c530859621571a674588
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-06-27 10:07:41 +0900

    [Gold IV] Title: 키 순서, Time: 656 ms, Memory: 49248 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -2,75 +2,62 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	static int N,M,tCnt,sCnt;
-	static int[][] arr;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int ans = 0;
-		int cnt = 0;
-		
-		st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N+1][N+1];
-		
-		for (int i=0;i<M;i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());	
-			arr[a][b] = 1;
-		}
-		
-		for (int i=1; i<N+1; i++) {
-			tCnt = 0;
-			sCnt = 0;
-			taller(i);
-			smaller(i);
-			if (tCnt + sCnt == N-1) cnt++;
-		}
-		System.out.println(cnt);
-	}
-	
-	static void taller(int start) {
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		q.offer(start);
-		boolean[] visited = new boolean[N+1];
-		visited[start] = true;
-		
-		while (!q.isEmpty()) {
-			int tmp = q.poll();
-			
-			for (int i=1; i<N+1; i++) {
-				if (arr[tmp][i] == 1 && !visited[i]) {
-					tCnt++;
-					visited[i] = true;
-					q.offer(i);
-				}
-			}	
-		}
-	}
-	
-	static void smaller(int start) {
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		q.offer(start);
-		boolean[] visited = new boolean[N+1];
-		visited[start] = true;
-		
-		while (!q.isEmpty()) {
-			int tmp = q.poll();
-			
-			for (int i=1; i<N+1; i++) {
-				if (arr[i][tmp] == 1 && !visited[i]) {
-					sCnt++;
-					visited[i] = true;
-					q.offer(i);
-				}
-			}
-		}
-	}
+  public static int N, M;
+  public static ArrayList<ArrayList<Integer>> arr1 = new ArrayList<ArrayList<Integer>>();
+  public static ArrayList<ArrayList<Integer>> arr2 = new ArrayList<ArrayList<Integer>>();
+  public static boolean[] visited;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    for (int i = 0; i <= N; i++) {
+      arr1.add(new ArrayList<>());
+      arr2.add(new ArrayList<>());
+    }
+
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      arr1.get(a).add(b);
+      arr2.get(b).add(a);
+    }
+
+    int cnt = 0;
+
+    for (int i = 1; i <= N; i++) {
+      int first = BFS(i, arr1);
+      int second = BFS(i, arr2);
+      if (first + second > N - 2)
+        cnt++;
+    }
+
+    System.out.println(cnt);
+  }
+
+  public static int BFS(int start, ArrayList<ArrayList<Integer>> arr) {
+    Queue<Integer> q = new ArrayDeque<>();
+    visited = new boolean[N + 1];
+    q.offer(start);
+    visited[start] = true;
+    int ans = 0;
+
+    while (!q.isEmpty()) {
+      int cur = q.poll();
+
+      for (int next : arr.get(cur)) {
+        if (!visited[next]) {
+          ans++;
+          visited[next] = true;
+          q.offer(next);
+        }
+      }
+    }
+    return ans;
+  }
 }

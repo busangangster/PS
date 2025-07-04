01fadbab3ac4af1972a1f99e67f07b9e45e06164
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-07-04 10:34:23 +0900

    [Gold III] Title: 다리 만들기, Time: 348 ms, Memory: 79916 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -1,123 +1,123 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-    static int N,islandNum,min_v;
-	static int[][] graph;
-	static boolean[][] visited;
-	static boolean[][] checked;
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {1,0,-1,0};
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+public class Main {
+  public static int N, number, ans;
+  public static int[][] arr, island;
+  public static int[] dx = { 0, 1, 0, -1 };
+  public static int[] dy = { 1, 0, -1, 0 };
+  public static boolean[][] visited;
 
-		N = Integer.parseInt(br.readLine());
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-		graph = new int[N][N];
-		visited = new boolean[N][N];
-		islandNum = 1;
-		min_v = Integer.MAX_VALUE;
+    N = Integer.parseInt(br.readLine());
 
-		for (int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j=0; j<N; j++) {
-				graph[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+    arr = new int[N][N];
+    island = new int[N][N];
+    number = 1;
+    ans = Integer.MAX_VALUE;
 
-		for (int i=0; i<N; i++) {
-			for (int j=0; j<N; j++) {
-				if (!visited[i][j] && graph[i][j] == 1) {
-					graph[i][j] = islandNum;
-					findIsland(i,j);
-					islandNum++;
-				}
-			}
-		}
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < N; j++) {
+        arr[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
 
-		for (int i=0; i<N; i++) {
-			for (int j=0; j<N; j++) {
-				if (graph[i][j] != 0) {
-					bfs(i,j);
-				}
-			}
-		}
-		System.out.println(min_v);
-	}
+    makeIsland();
+    makeBridge();
+    System.out.println(ans);
+  }
 
-	static void bfs(int x, int y){
-		Queue<Node> q = new ArrayDeque<>();
-		checked = new boolean[N][N];
-		checked[x][y] = true;
-		q.offer(new Node(x,y,0));
+  public static void makeBridge() {
+    for (int i = 1; i <= number; i++) {
+      bridge(i);
+    }
+  }
 
-		while (!q.isEmpty()){
-			Node cur = q.poll();
+  public static void bridge(int target) {
+    Queue<Node> q = new ArrayDeque<>();
+    visited = new boolean[N][N];
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (island[i][j] == target) {
+          visited[i][j] = true;
+          q.offer(new Node(i, j, 0));
+        }
+      }
+    }
 
-			if (cur.cnt > min_v) {
-				continue;
-			}
+    while (!q.isEmpty()) {
+      Node cur = q.poll();
 
-			if (graph[cur.x][cur.y] != 0 && graph[cur.x][cur.y] != graph[x][y]) {
-				min_v = Math.min(cur.cnt-1,min_v);
-			}
+      for (int i = 0; i < 4; i++) {
+        int nx = cur.x + dx[i];
+        int ny = cur.y + dy[i];
 
-			for (int i=0; i<4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
+        if (check(nx, ny) && !visited[nx][ny]) {
+          if (island[nx][ny] == 0) {
+            q.offer(new Node(nx, ny, cur.dis + 1));
+            visited[nx][ny] = true;
+          } else if (island[nx][ny] != target) {
+            ans = Math.min(ans, cur.dis);
+            return;
+          }
+        }
+      }
+    }
+  }
 
-				if (!check(nx, ny)) continue;
-				if (checked[nx][ny]) continue;
-				if (graph[nx][ny] == graph[x][y]) continue;
+  public static void makeIsland() {
+    visited = new boolean[N][N];
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (arr[i][j] == 1 && !visited[i][j]) {
+          bfs(i, j);
+          number++;
+        }
+      }
+    }
+  }
 
-				q.offer(new Node(nx,ny,cur.cnt+1));
-				checked[nx][ny] = true;
-				
-			}
-		}
+  public static void bfs(int x, int y) {
+    Queue<Node> q = new ArrayDeque<>();
+    visited[x][y] = true;
+    island[x][y] = number;
+    q.offer(new Node(x, y, 0));
 
-	}
+    while (!q.isEmpty()) {
+      Node cur = q.poll();
 
-	static void findIsland(int x, int y) {
-		Queue<Node> q = new ArrayDeque<>();
-		visited[x][y] = true;
-		q.offer(new Node(x,y));
+      for (int i = 0; i < 4; i++) {
+        int nx = cur.x + dx[i];
+        int ny = cur.y + dy[i];
 
-		while (!q.isEmpty()) {
-			Node cur = q.poll();
+        if (check(nx, ny) && !visited[nx][ny]) {
+          if (arr[nx][ny] == 1) {
+            island[nx][ny] = number;
+            q.offer(new Node(nx, ny, 0));
+            visited[nx][ny] = true;
+          }
+        }
+      }
+    }
+  }
 
-			for (int i=0; i <4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
+  public static boolean check(int x, int y) {
+    return (0 <= x && x < N && 0 <= y && y < N);
+  }
+}
 
-				if (!check(nx,ny)) continue;
-				if (visited[nx][ny]) continue;
+class Node {
+  int x;
+  int y;
+  int dis;
 
-				if (graph[nx][ny] == 1) {
-					graph[nx][ny] = islandNum;
-					visited[nx][ny] = true;
-					q.offer(new Node(nx,ny));
-				}
-			}
-		}
-	}
-
-	static boolean check(int x, int y){
-		if (0 <= x && x < N && 0 <= y && y < N) return true;
-		else return false;
-	}
-
-	static class Node{
-		int x,y,cnt;
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-		public Node(int x, int y, int cnt) {
-			this.x = x;
-			this.y = y;
-			this.cnt = cnt;
-		}
-	}
+  public Node(int x, int y, int dis) {
+    this.x = x;
+    this.y = y;
+    this.dis = dis;
+  }
 }

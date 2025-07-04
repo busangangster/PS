1b6eeb0f33ff51d9b8615b8abbcb0ad41ae52955
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-07-04 10:38:00 +0900

    [Gold III] Title: 다리 만들기, Time: 332 ms, Memory: 79268 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-  public static int N, number, ans;
+  public static int N, islandNum, ans;
   public static int[][] arr, island;
   public static int[] dx = { 0, 1, 0, -1 };
   public static int[] dy = { 1, 0, -1, 0 };
@@ -13,11 +13,8 @@ public class Main {
     StringTokenizer st;
 
     N = Integer.parseInt(br.readLine());
-
     arr = new int[N][N];
     island = new int[N][N];
-    number = 1;
-    ans = Integer.MAX_VALUE;
 
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
@@ -26,65 +23,32 @@ public class Main {
       }
     }
 
-    makeIsland();
-    makeBridge();
-    System.out.println(ans);
-  }
-
-  public static void makeBridge() {
-    for (int i = 1; i <= number; i++) {
-      bridge(i);
-    }
-  }
-
-  public static void bridge(int target) {
-    Queue<Node> q = new ArrayDeque<>();
     visited = new boolean[N][N];
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        if (island[i][j] == target) {
-          visited[i][j] = true;
-          q.offer(new Node(i, j, 0));
-        }
-      }
-    }
-
-    while (!q.isEmpty()) {
-      Node cur = q.poll();
-
-      for (int i = 0; i < 4; i++) {
-        int nx = cur.x + dx[i];
-        int ny = cur.y + dy[i];
-
-        if (check(nx, ny) && !visited[nx][ny]) {
-          if (island[nx][ny] == 0) {
-            q.offer(new Node(nx, ny, cur.dis + 1));
-            visited[nx][ny] = true;
-          } else if (island[nx][ny] != target) {
-            ans = Math.min(ans, cur.dis);
-            return;
-          }
-        }
-      }
-    }
-  }
-
-  public static void makeIsland() {
-    visited = new boolean[N][N];
+    islandNum = 1;
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
         if (arr[i][j] == 1 && !visited[i][j]) {
           bfs(i, j);
-          number++;
+          islandNum++;
         }
       }
     }
+
+    visited = new boolean[N][N];
+    ans = Integer.MAX_VALUE;
+
+    for (int i = 1; i <= islandNum; i++) {
+      move(i);
+    }
+
+    System.out.println(ans);
+
   }
 
   public static void bfs(int x, int y) {
-    Queue<Node> q = new ArrayDeque<>();
+    Queue<Node> q = new ArrayDeque<Node>();
     visited[x][y] = true;
-    island[x][y] = number;
+    island[x][y] = islandNum;
     q.offer(new Node(x, y, 0));
 
     while (!q.isEmpty()) {
@@ -94,15 +58,50 @@ public class Main {
         int nx = cur.x + dx[i];
         int ny = cur.y + dy[i];
 
-        if (check(nx, ny) && !visited[nx][ny]) {
-          if (arr[nx][ny] == 1) {
-            island[nx][ny] = number;
+        if (check(nx, ny)) {
+          if (!visited[nx][ny] && arr[nx][ny] == 1) {
             q.offer(new Node(nx, ny, 0));
+            island[nx][ny] = islandNum;
             visited[nx][ny] = true;
           }
         }
       }
     }
+  }
+
+  public static void move(int target) {
+    Queue<Node> q = new ArrayDeque<>();
+    visited = new boolean[N][N];
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (island[i][j] == target) {
+          q.offer(new Node(i, j, 0));
+          visited[i][j] = true;
+        }
+      }
+    }
+
+    while (!q.isEmpty()) {
+      Node cur = q.poll();
+
+      for (int i = 0; i < 4; i++) {
+        int nx = cur.x + dx[i];
+        int ny = cur.y + dy[i];
+
+        if (check(nx, ny)) {
+          if (!visited[nx][ny]) {
+            if (island[nx][ny] == 0) {
+              visited[nx][ny] = true;
+              q.offer(new Node(nx, ny, cur.cnt + 1));
+            } else {
+              ans = Math.min(ans, cur.cnt);
+              return;
+            }
+          }
+        }
+      }
+    }
+
   }
 
   public static boolean check(int x, int y) {
@@ -113,11 +112,11 @@ public class Main {
 class Node {
   int x;
   int y;
-  int dis;
+  int cnt;
 
-  public Node(int x, int y, int dis) {
+  public Node(int x, int y, int cnt) {
     this.x = x;
     this.y = y;
-    this.dis = dis;
+    this.cnt = cnt;
   }
 }

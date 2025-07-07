13a2079398ae270dac9925cd53ab8b94fcfb6695
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-07-07 15:14:35 +0900

    [Silver I] Title: 컴백홈, Time: 112 ms, Memory: 14956 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.java
+++ 백준/Silver/1189. 컴백홈/컴백홈.java
@@ -1,67 +1,63 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main {
-	static int R,C,K,ans;
-	static char[][] arr;
-	
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {1,0,-1,0};
-	static boolean[][] visited;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
+  public static int N, M, K, ans;
+  public static char[][] arr;
+  public static boolean[][] visited;
+  public static int[] dx = { 0, 1, 0, -1 };
+  public static int[] dy = { 1, 0, -1, 0 };
 
-		arr = new char[R][C];
-		visited= new boolean[R][C];
-		ans = 0;
-		
-		for (int i=0; i<R; i++) {
-			String s = br.readLine();
-			for (int j=0; j<C; j++) {
-				arr[i][j] = s.charAt(j);
-			}
-		}
-		
-		visited[R-1][0] = true;
-		dfs(R-1,0,1);
-		System.out.println(ans);
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-	}
-	
-	static void dfs(int x, int y, int cnt) {
-		if (x == 0 && y == C-1) {
-			if (cnt == K) {
-				ans++;
-			}
-			return;
-		}
-		
-		for (int i=0; i<4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			
-			if (!check(nx,ny)) continue;
-			if (arr[nx][ny] == 'T') continue;
-			if (visited[nx][ny]) continue;
-			
-			visited[nx][ny] = true;
-			dfs(nx,ny,cnt+1);
-			visited[nx][ny] = false;		
-			
-		}	
-		
-	}
-	
-	static boolean check(int x, int y) {
-		if (0 <= x && x < R && 0 <= y && y < C) return true;
-		else return false;
-	}
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
 
+    arr = new char[N][M];
+    visited = new boolean[N][M];
+    ans = 0;
+
+    for (int i = 0; i < N; i++) {
+      String s = br.readLine();
+      for (int j = 0; j < M; j++) {
+        arr[i][j] = s.charAt(j);
+      }
+    }
+
+    visited[N - 1][0] = true;
+    dfs(N - 1, 0, 1);
+
+    System.out.println(ans);
+  }
+
+  public static void dfs(int x, int y, int cnt) {
+    if (x == 0 && y == M - 1) {
+      if (cnt == K) {
+        ans++;
+      }
+      return;
+    }
+
+    for (int i = 0; i < 4; i++) {
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+
+      if (check(nx, ny)) {
+        if (!visited[nx][ny] && arr[nx][ny] == '.') {
+          visited[nx][ny] = true;
+          dfs(nx, ny, cnt + 1);
+          visited[nx][ny] = false;
+        }
+      }
+    }
+
+  }
+
+  public static boolean check(int x, int y) {
+    return (0 <= x && x < N && 0 <= y && y < M);
+  }
 }

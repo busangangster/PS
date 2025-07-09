a6085644df44073349057931e0cecaea0b6b34ef
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-07-09 11:51:28 +0900

    [Silver IV] Title: 상근이의 여행, Time: 308 ms, Memory: 45280 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -2,9 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-  public static int N, M;
-  public static boolean[] visited;
-  public static ArrayList<ArrayList<Integer>> arr;
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,21 +12,13 @@ public class Main {
 
     while (t-- > 0) {
       st = new StringTokenizer(br.readLine());
-      N = Integer.parseInt(st.nextToken());
-      M = Integer.parseInt(st.nextToken());
-
-      arr = new ArrayList<ArrayList<Integer>>();
-      for (int i = 0; i <= N; i++) {
-        arr.add(new ArrayList<>());
-      }
-      visited = new boolean[N + 1];
+      int N = Integer.parseInt(st.nextToken());
+      int M = Integer.parseInt(st.nextToken());
 
       for (int i = 0; i < M; i++) {
         st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        arr.get(a).add(b);
-        arr.get(b).add(a);
       }
 
       sb.append(N - 1).append("\n");

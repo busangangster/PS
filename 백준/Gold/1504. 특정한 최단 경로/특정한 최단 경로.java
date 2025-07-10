59e707f4ba3a67b94c4ffc578819cdb39ebedf86
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-07-10 14:46:09 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 612 ms, Memory: 68940 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,88 +1,89 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+  public static int N, E, v1, v2, INF;
+  public static ArrayList<ArrayList<Node>> arr = new ArrayList<ArrayList<Node>>();
 
-    static int N, E, INF;
-    static ArrayList<ArrayList<Node>> arr = new ArrayList<ArrayList<Node>>();
-    static int[] min_dis;
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    E = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+    for (int i = 0; i <= N; i++) {
+      arr.add(new ArrayList<>());
+    }
+    INF = 200000 * 1000 + 1;
 
-        INF = 800 * 200000 + 1;
-
-        for (int i = 0; i < N + 1; i++) {
-            arr.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            arr.get(u).add(new Node(v, c));
-            arr.get(v).add(new Node(u, c));
-        }
-
-        st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
-        
-        int[] start = dijkstra(1);
-        int[] d1 = dijkstra(v1);
-        int[] d2 = dijkstra(v2);
-
-        int first = start[v1] + d1[v2] + d2[N];
-        int second = start[v2] + d2[v1] + d1[N];
-
-        if (first >= INF || second >= INF) {
-            System.out.println(-1);
-        }
-        else {
-            System.out.println(Integer.min(first, second));
-        }
+    for (int i = 0; i < E; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      int c = Integer.parseInt(st.nextToken());
+      arr.get(a).add(new Node(b, c));
+      arr.get(b).add(new Node(a, c));
     }
 
-    public static int[] dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<Node>((o1, o2) -> o1.cost - o2.cost);
-        min_dis = new int[N + 1];
-        Arrays.fill(min_dis, INF);
+    st = new StringTokenizer(br.readLine());
+    v1 = Integer.parseInt(st.nextToken());
+    v2 = Integer.parseInt(st.nextToken());
 
-        min_dis[start] = 0;
-        pq.offer(new Node(start, 0));
+    int[] fromStart = dijkstra(1);
+    int[] fromV1 = dijkstra(v1);
+    int[] fromV2 = dijkstra(v2);
 
-        while (!pq.isEmpty()) {
-            Node cur = pq.poll();
+    int firstDistance = 0;
+    int secondDistance = 0;
 
-            if (min_dis[cur.idx] < cur.cost)
-                continue;
+    firstDistance += fromStart[v1];
+    firstDistance += fromV1[v2];
+    firstDistance += fromV2[N];
 
-            for (int i = 0; i < arr.get(cur.idx).size(); i++) {
-                Node next = arr.get(cur.idx).get(i);
-                if (min_dis[next.idx] > cur.cost + next.cost) {
-                    min_dis[next.idx] = cur.cost + next.cost;
-                    pq.offer(new Node(next.idx, min_dis[next.idx]));
-                }
-            }
-        }
-        return min_dis;
+    secondDistance += fromStart[v2];
+    secondDistance += fromV2[v1];
+    secondDistance += fromV1[N];
+
+    if (firstDistance >= INF && secondDistance >= INF) {
+      System.out.println(-1);
+    } else {
+      System.out.println(Math.min(firstDistance, secondDistance));
     }
-    
+
+  }
+
+  public static int[] dijkstra(int start) {
+    PriorityQueue<Node> pq = new PriorityQueue<Node>((o1, o2) -> o1.cost - o2.cost);
+    int[] min_dis = new int[N + 1];
+    Arrays.fill(min_dis, INF);
+    min_dis[start] = 0;
+    pq.offer(new Node(start, 0));
+
+    while (!pq.isEmpty()) {
+      Node cur = pq.poll();
+
+      if (min_dis[cur.node] < cur.cost)
+        continue;
+
+      for (Node next : arr.get(cur.node)) {
+        if (min_dis[next.node] > cur.cost + next.cost) {
+          min_dis[next.node] = cur.cost + next.cost;
+          pq.offer(new Node(next.node, cur.cost + next.cost));
+        }
+      }
+    }
+    return min_dis;
+  }
 }
 
 class Node {
-    int idx;
-    int cost;
+  int node;
+  int cost;
 
-    public Node(int idx, int cost) {
-        this.idx = idx;
-        this.cost = cost;
-    }
+  public Node(int node, int cost) {
+    this.node = node;
+    this.cost = cost;
+  }
 }

339f26f734064a3a4d3ebc936300bc582e481ebb
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-07-15 12:47:48 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 152 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -1,45 +1,49 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main { 
+public class Main {
 
-	public static void main(String[] args)  throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(br.readLine());
+    int n = Integer.parseInt(br.readLine());
 
-		int[] arr = new int[n];
-		int[] dp = new int[n];
+    int[] dp = new int[n];
+    int[] arr = new int[n];
 
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
 
-		for (int i=0; i<n; i++) {
-			dp[i] = 1;
-			for (int j=0; j<i; j++) {
-				if (arr[i] > arr[j]) {
-					dp[i] = Math.max(dp[i],dp[j]+1);
-				}
-			}
-		}
-		int len = Arrays.stream(dp).max().getAsInt();
+    for (int i = 0; i < n; i++) {
+      dp[i] = 1;
+      for (int j = 0; j < i; j++) {
+        if (arr[i] > arr[j]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+      }
+    }
 
-		int cnt = len;
-		Deque<Integer> d = new ArrayDeque<>();
-		for (int i=n-1; i>=0; i--) {
-			if (dp[i] == cnt) {
-				d.addFirst(arr[i]);
-				cnt--;
-			}
-		}
-		for (int x: d){
-			sb.append(x).append(" ");
-		}
-		System.out.println(len);
-		System.out.println(sb);
-	}
+    int len = Arrays.stream(dp).max().getAsInt();
+    int cnt = len;
+
+    Deque<Integer> dq = new ArrayDeque<>();
+    for (int i = n - 1; i >= 0; i--) {
+      if (dp[i] == cnt) {
+        dq.addFirst(arr[i]);
+        cnt--;
+      }
+    }
+
+    sb.append(len).append("\n");
+    while (!dq.isEmpty()) {
+      sb.append(dq.pollFirst()).append(" ");
+    }
+
+    System.out.println(sb);
+
+  }
 }

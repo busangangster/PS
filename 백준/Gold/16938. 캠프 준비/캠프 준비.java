96c05c29b013ac8068e1ff2f92c86b41a23b457c
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-07-23 14:17:59 +0900

    [Gold V] Title: 캠프 준비, Time: 140 ms, Memory: 15684 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.java
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.java
@@ -1,67 +1,68 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N,L,R,X,ans;
-	static int[] level;
-	static boolean[] selected;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		X = Integer.parseInt(st.nextToken());
-		
-		
-		level = new int[N];
-		ans = 0;
-		selected = new boolean[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<N; i++) {
-			level[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		subset(0);
-		System.out.println(ans);
+  public static int N, L, R, X, ans;
+  public static boolean[] selected;
+  public static int[] problems;
 
-	}
-	
-	static void subset(int cnt) {
-		if (cnt == N) {
-			ArrayList<Integer> arr = new ArrayList<>();
-			int tmp = 0;
-			for (int i=0; i<N; i++) {
-				if (selected[i]) {
-					arr.add(level[i]);
-					tmp += level[i];
-				}
-			}
-			if (arr.size() <= 1) {
-				return;
-			}
-			
-			if (tmp > R || tmp < L) {
-				return;
-			}
-			
-			if (Collections.max(arr) - Collections.min(arr) < X) {
-				return;
-			}
-			
-			ans++;
-			return;
-		}
-		
-		selected[cnt] = true;
-		subset(cnt+1);
-		selected[cnt] = false;
-		subset(cnt+1);
-		
-	}
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    L = Integer.parseInt(st.nextToken());
+    R = Integer.parseInt(st.nextToken());
+    X = Integer.parseInt(st.nextToken());
+
+    problems = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      problems[i] = Integer.parseInt(st.nextToken());
+    }
+
+    selected = new boolean[N];
+    ans = 0;
+
+    subset(0);
+    System.out.println(ans);
+  }
+
+  public static void subset(int cnt) {
+    if (cnt == N) {
+      if (cal())
+        ans++;
+      return;
+    }
+
+    selected[cnt] = true;
+    subset(cnt + 1);
+
+    selected[cnt] = false;
+    subset(cnt + 1);
+  }
+
+  public static boolean cal() {
+    int sum = 0;
+    int getTrue = 0;
+    int hardest = Integer.MIN_VALUE;
+    int easiest = Integer.MAX_VALUE;
+    for (int i = 0; i < N; i++) {
+      if (selected[i]) {
+        sum += problems[i];
+        getTrue++;
+        hardest = Math.max(hardest, problems[i]);
+        easiest = Math.min(easiest, problems[i]);
+      }
+    }
+    if (getTrue < 2)
+      return false;
+    if (hardest - easiest < X)
+      return false;
+    if (sum < L || sum > R)
+      return false;
+
+    return true;
+  }
 }

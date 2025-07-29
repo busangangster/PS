0dc05ad157b35587de9c73d07b6081045601d103
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-07-29 13:52:09 +0900

    [Silver I] Title: 경로 찾기, Time: 184 ms, Memory: 17336 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,43 +1,65 @@
-import java.io.*;
 import java.util.*;
- 
-class Main {
-	static int N;
-	static int[][] map;
-	static ArrayList<ArrayList<Integer>> arr;
+import java.io.*;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+public class Main {
+  public static int N;
+  public static ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
+  public static boolean[] visited;
 
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
 
-		for (int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j=0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		for (int k=0; k<N; k++) {
-			for (int i=0; i<N; i++) {
-				for (int j=0; j<N; j++) {
-					if (map[i][k]==1 && map[k][j]==1) {
-						map[i][j] = 1;
-					}
-				}
-			}
-		}
-		for (int[] x : map) {
-			for (int val : x ) {
-				sb.append(val).append(" ");
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb);
-	}
+    N = Integer.parseInt(br.readLine());
 
-	
+    for (int i = 0; i < N; i++) {
+      arr.add(new ArrayList<>());
+    }
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < N; j++) {
+        int tmp = Integer.parseInt(st.nextToken());
+        if (i == j)
+          continue;
+
+        if (tmp == 1) {
+          arr.get(i).add(j);
+        }
+      }
+    }
+
+    for (int i = 0; i < N; i++) {
+      visited = new boolean[N];
+      bfs(i);
+      for (int j = 0; j < N; j++) {
+        if (visited[j])
+          sb.append(1);
+
+        else
+          sb.append(0);
+        sb.append(" ");
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static void bfs(int start) {
+    Queue<Integer> q = new ArrayDeque<>();
+    q.offer(start);
+
+    while (!q.isEmpty()) {
+      int cur = q.poll();
+
+      for (int next : arr.get(cur)) {
+        if (!visited[next]) {
+          visited[next] = true;
+          q.offer(next);
+        }
+      }
+    }
+
+  }
 }

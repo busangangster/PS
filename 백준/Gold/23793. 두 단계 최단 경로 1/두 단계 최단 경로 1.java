64deb07ef8af3eaf0d5e73a7eb9a87fda139c9a2
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-08-07 12:44:34 +0900

    [Gold IV] Title: 두 단계 최단 경로 1, Time: 1048 ms, Memory: 104148 KB -BaekjoonHub

--- 백준/Gold/23793. 두 단계 최단 경로 1/두 단계 최단 경로 1.java
+++ 백준/Gold/23793. 두 단계 최단 경로 1/두 단계 최단 경로 1.java
@@ -1,115 +1,108 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	static int N, M, INF, y;
-	static int[] min_dis;
-	static ArrayList<ArrayList<Node>> arr;
+public class Main {
+  public static int N, M, INF, x, y, z;
+  public static ArrayList<ArrayList<Node>> arr;
+  public static int[] min_dis1, min_dis2, min_dis3;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
 
-		arr = new ArrayList<ArrayList<Node>>();
-		INF = 10000 * 200000 + 1;
+    arr = new ArrayList<ArrayList<Node>>();
+    for (int i = 0; i <= N; i++) {
+      arr.add(new ArrayList<>());
+    }
 
-		for (int i = 0; i <= N; i++) {
-			arr.add(new ArrayList<>());
-		}
+    INF = 10000 * 200000 + 1;
+    min_dis1 = new int[N + 1];
+    min_dis2 = new int[N + 1];
+    min_dis3 = new int[N + 1];
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			arr.get(u).add(new Node(v, w));
-		}
+    Arrays.fill(min_dis1, INF);
+    Arrays.fill(min_dis2, INF);
+    Arrays.fill(min_dis3, INF);
 
-		st = new StringTokenizer(br.readLine());
-		int x = Integer.parseInt(st.nextToken());
-		y = Integer.parseInt(st.nextToken());
-		int z = Integer.parseInt(st.nextToken());
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      int c = Integer.parseInt(st.nextToken());
+      arr.get(a).add(new Node(b, c));
+    }
 
-		int[] toY = dijkstra(x);
-		int[] toZ = dijkstra(y);
+    st = new StringTokenizer(br.readLine());
+    x = Integer.parseInt(st.nextToken());
+    y = Integer.parseInt(st.nextToken());
+    z = Integer.parseInt(st.nextToken());
 
-		if (toY[y] == INF || toZ[z] == INF) {
-			sb.append(-1).append(" ");
-		} else {
-			sb.append(toY[y] + toZ[z]).append(" ");
-		}
+    int[] fromx = dijkstra1(x, min_dis1);
+    int[] fromy = dijkstra1(y, min_dis2);
+    int[] whole = dijkstra2(x, min_dis3);
 
-		int[] tmp = dijkstra2(x);
-		long second = tmp[z];
-		sb.append(second == INF ? -1 : second);
-		System.out.println(sb);
-	}
+    sb.append(fromx[y] == INF || fromy[z] == INF ? -1 : fromx[y] + fromy[z]).append(" ");
+    sb.append(whole[z] == INF ? -1 : whole[z]);
+    System.out.println(sb);
 
-	static int[] dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(start, 0));
-		min_dis = new int[N + 1];
-		Arrays.fill(min_dis, INF);
-		min_dis[start] = 0;
+  }
 
-		while (!pq.isEmpty()) {
-			Node cur = pq.poll();
+  public static int[] dijkstra1(int start, int[] min_dis) {
+    PriorityQueue<Node> pq = new PriorityQueue<Node>((o1, o2) -> o1.cost - o2.cost);
+    min_dis[start] = 0;
+    pq.offer(new Node(start, 0));
 
-			if (min_dis[cur.node] < cur.cost)
-				continue;
+    while (!pq.isEmpty()) {
+      Node cur = pq.poll();
 
-			for (Node next : arr.get(cur.node)) {
-				if (min_dis[next.node] > cur.cost + next.cost) {
-					min_dis[next.node] = cur.cost + next.cost;
-					pq.offer(new Node(next.node, cur.cost + next.cost));
-				}
-			}
-		}
-		return min_dis;
-	}
+      if (min_dis[cur.node] < cur.cost)
+        continue;
 
-	static int[] dijkstra2(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(start, 0));
-		min_dis = new int[N + 1];
-		Arrays.fill(min_dis, INF);
-		min_dis[start] = 0;
+      for (Node next : arr.get(cur.node)) {
+        if (min_dis[next.node] > cur.cost + next.cost) {
+          min_dis[next.node] = cur.cost + next.cost;
+          pq.offer(new Node(next.node, cur.cost + next.cost));
+        }
+      }
+    }
+    return min_dis;
+  }
 
-		while (!pq.isEmpty()) {
-			Node cur = pq.poll();
+  public static int[] dijkstra2(int start, int[] min_dis) {
+    PriorityQueue<Node> pq = new PriorityQueue<Node>((o1, o2) -> o1.cost - o2.cost);
+    min_dis[start] = 0;
+    pq.offer(new Node(start, 0));
 
-			if (min_dis[cur.node] < cur.cost)
-				continue;
+    while (!pq.isEmpty()) {
+      Node cur = pq.poll();
 
-			for (Node next : arr.get(cur.node)) {
-				if (next.node == y)
-					continue;
-				if (min_dis[next.node] > cur.cost + next.cost) {
-					min_dis[next.node] = cur.cost + next.cost;
-					pq.offer(new Node(next.node, cur.cost + next.cost));
-				}
-			}
-		}
-		return min_dis;
-	}
+      if (min_dis[cur.node] < cur.cost)
+        continue;
 
-	static class Node implements Comparable<Node> {
-		int node, cost;
+      for (Node next : arr.get(cur.node)) {
+        if (next.node != y) {
+          if (min_dis[next.node] > cur.cost + next.cost) {
+            min_dis[next.node] = cur.cost + next.cost;
+            pq.offer(new Node(next.node, cur.cost + next.cost));
+          }
+        }
+      }
+    }
+    return min_dis;
+  }
+}
 
-		public Node(int node, int cost) {
-			this.node = node;
-			this.cost = cost;
-		}
+class Node {
+  int node;
+  int cost;
 
-		@Override
-		public int compareTo(Node o) {
-			return this.cost - o.cost;
-		}
-	}
-
+  public Node(int node, int cost) {
+    this.node = node;
+    this.cost = cost;
+  }
 }

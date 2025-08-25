adfbf1175c5974fdd6793f0dad7ef016bedc218d
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-08-25 13:35:48 +0900

    [Silver III] Title: 근손실, Time: 112 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.java
+++ 백준/Silver/18429. 근손실/근손실.java
@@ -2,50 +2,50 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N,K,ans;
-	static int[] kit,used;
-	static boolean[] checked;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		kit = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<N; i++) {
-			kit[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		used = new int[N];
-		checked = new boolean[N];
-		ans = 0;
-		
-		perm(0,500);
-		System.out.println(ans);
-	}
-	
-	static void perm(int cnt, int sum) {
+  public static int N, K, ans;
+  public static int[] kits;
+  public static int[] selected;
+  public static boolean[] visited;
 
-		if (cnt == N) {
-			ans++;
-			return;
-		}
-		
-		for (int i=0; i<N; i++) {
-			
-			if (checked[i]) continue;
-			
-			if (sum+kit[i] - K >= 500) {
-				checked[i] = true;
-				perm(cnt+1,sum+kit[i]-K);
-				checked[i] = false;
-			}
-			
-		}
-	}	
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+
+    visited = new boolean[N];
+    selected = new int[N];
+    kits = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      kits[i] = Integer.parseInt(st.nextToken());
+    }
+
+    ans = 0;
+    dfs(0, 500);
+    System.out.println(ans);
+
+  }
+
+  public static void dfs(int cnt, int sum) {
+    if (cnt == N) {
+      ans++;
+      return;
+    }
+    if (sum < 500) {
+      sum = 500;
+      return;
+    }
+
+    for (int i = 0; i < N; i++) {
+      if (visited[i])
+        continue;
+
+      visited[i] = true;
+      dfs(cnt + 1, sum + kits[i] - K);
+      visited[i] = false;
+    }
+  }
 }

4e7381adae5896bfdfd6e536b98cfece717bd613
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-04 10:48:23 +0900

    [Gold IV] Title: 탈출, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,119 +1,119 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	static int R,C,target_x,target_y;
-	static Queue<Pos> hedgehog,water;
-	static boolean[][] visited1, visited2;
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	static char[][] graph;
-	
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        
-        graph = new char[R][C];
-        visited1 = new boolean[R][C];
-        visited2 = new boolean[R][C];
-        hedgehog = new ArrayDeque<>();
-        water = new ArrayDeque<>();
-        
-        for (int i=0; i<R; i++) {
-        	String s = br.readLine();
-        	for (int j=0; j<C; j++) {
-        		if (s.charAt(j) == '*') {
-        			water.add(new Pos(i,j));
-        			visited1[i][j] = true;
-        			
-        		}
-        		else if (s.charAt(j) == 'S') {
-        			hedgehog.add(new Pos(i,j));
-        			visited2[i][j] = true;
-        			graph[i][j] = '.';
-        		}
-        		else {
-        			if (s.charAt(j) == 'D') {
-        				target_x = i;
-        				target_y = j;
-        			}
-        			graph[i][j] = s.charAt(j);
-        		}
-        	}
+public class Main {
+  public static int N, M, sx, sy, tx, ty;
+  public static char[][] arr;
+  public static int[][] time;
+  public static boolean[][] visited;
+  public static Queue<Node> water = new ArrayDeque<Node>();
+  public static int[] dx = { 0, 1, 0, -1 };
+  public static int[] dy = { 1, 0, -1, 0 };
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    arr = new char[N][M];
+    time = new int[N][M];
+
+    for (int[] t : time) {
+      Arrays.fill(t, 100000);
+    }
+
+    visited = new boolean[N][M];
+
+    for (int i = 0; i < N; i++) {
+      String s = br.readLine();
+      for (int j = 0; j < M; j++) {
+        char tmp = s.charAt(j);
+        if (tmp == '*') {
+          water.add(new Node(i, j, 0));
+          visited[i][j] = true;
+        } else if (tmp == 'D') {
+          tx = i;
+          ty = j;
+        } else if (tmp == 'S') {
+          sx = i;
+          sy = j;
         }
-        
-        int ans = solve();
-        
-        System.out.println(ans == -1? "KAKTUS" : ans );
-    	
+        arr[i][j] = tmp;
+      }
     }
-    
-    public static int solve() {
-		int tmp = 0;
-		
-    	while (!hedgehog.isEmpty()) {
-    		
 
-    		
-    		int hog = hedgehog.size();
-    		int w = water.size();
-    		
-    		for (int i=0; i<w; i++) {
-    			Pos cur = water.poll();
-    			
-    			for (int j=0; j<4; j++) {
-    				int nx = cur.x + dx[j];
-    				int ny = cur.y + dy[j];
-    				
-    				if (!check(nx,ny)) continue;
-    				if (visited1[nx][ny]) continue;
-    				if (graph[nx][ny] == 'X' || graph[nx][ny] == 'D') continue;
-    				
-    				graph[nx][ny] = '*';
-    				visited1[nx][ny] = true;
-    				water.offer(new Pos(nx,ny));
-    			}
-    		}
-    		
-    		for (int i=0; i< hog; i++) {
-    			Pos cur = hedgehog.poll();
-    			
-    			for (int j=0; j<4; j++) {
-    				int nx = cur.x + dx[j];
-    				int ny = cur.y + dy[j];
-    				
-    				if (nx == target_x && ny == target_y) {
-    					return tmp+1;
-    				}
-    				if (!check(nx,ny)) continue;
-    				if (visited2[nx][ny]) continue;
-    				if (graph[nx][ny] == 'X' || graph[nx][ny] == '*') continue;
-    				
-    				visited2[nx][ny] = true;
-    				hedgehog.offer(new Pos(nx,ny));
+    waterMove();
+    visited = new boolean[N][M];
+    int res = move();
 
-    			}
-    		}
-    		tmp++;
-    	}
-    	return -1;
-    	
+    System.out.println(res == -1 ? "KAKTUS" : res);
+
+  }
+
+  public static void waterMove() {
+    while (!water.isEmpty()) {
+      Node cur = water.poll();
+
+      for (int i = 0; i < 4; i++) {
+        int nx = cur.x + dx[i];
+        int ny = cur.y + dy[i];
+
+        if (check(nx, ny)) {
+          if (!visited[nx][ny] && arr[nx][ny] != 'X' && arr[nx][ny] != 'D') {
+            visited[nx][ny] = true;
+            time[nx][ny] = cur.t + 1;
+            water.add(new Node(nx, ny, cur.t + 1));
+          }
+        }
+      }
     }
-    
-    public static boolean check(int x, int y) {
-    	if (0 <= x &&  x < R && 0 <= y && y < C ) return true;
-    	else return false;
+  }
+
+  public static int move() {
+    Queue<Node> q = new ArrayDeque<>();
+    q.offer(new Node(sx, sy, 0));
+    visited[sx][sy] = true;
+
+    while (!q.isEmpty()) {
+      Node cur = q.poll();
+
+      if (cur.x == tx && cur.y == ty) {
+        return cur.t;
+      }
+
+      for (int i = 0; i < 4; i++) {
+        int nx = cur.x + dx[i];
+        int ny = cur.y + dy[i];
+
+        if (check(nx, ny)) {
+          if (!visited[nx][ny] && arr[nx][ny] != 'X' && arr[nx][ny] != '*') {
+            if (cur.t + 1 < time[nx][ny]) {
+              q.offer(new Node(nx, ny, cur.t + 1));
+              visited[nx][ny] = true;
+            }
+          }
+        }
+      }
     }
+    return -1;
+  }
+
+  public static boolean check(int x, int y) {
+    return (0 <= x && x < N && 0 <= y && y < M);
+  }
 }
 
-class Pos{
-	int x,y;
-	public Pos(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+class Node {
+  int x;
+  int y;
+  int t;
+
+  public Node(int x, int y, int t) {
+    this.x = x;
+    this.y = y;
+    this.t = t;
+  }
 }

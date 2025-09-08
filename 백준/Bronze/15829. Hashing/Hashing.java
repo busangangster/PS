d4314de06f2ffb6fd3504a348ca642f5a8c15e72
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-08 11:27:59 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14116 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,25 +1,21 @@
 import java.util.*;
 import java.io.*;
-import java.math.BigInteger;
 
 public class Main {
- public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		String s = br.readLine();
-		long ans = 0;
-		int r = 31;
-		int mod = 1234567891;
-		long pow = 1;
-		
-		for (int i=0; i<n; i++) {
-			int tmp = s.charAt(i) - 96;
-			ans += (tmp * pow);
-			pow = (pow*31) % mod;
-		}
-		sb.append(ans%mod);
-		System.out.println(sb);
- }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    long res = 0L;
+    int mod = 1234567891;
+
+    int n = Integer.parseInt(br.readLine());
+    String s = br.readLine();
+    for (int i = 0; i < n; i++) {
+      int tmp = (int) s.charAt(i) - 96;
+      res += tmp * (Math.pow(31, i) % mod);
+      res %= mod;
+    }
+    System.out.println(res % mod);
+  }
 }

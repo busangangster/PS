9ff98b0ef1ae1cc8713122b49b19f5f1a48f5686
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-08 12:51:44 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 16116 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,48 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N,M,res,answer;
-	static int[] nums,ans;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		ans = new int[3];
-		nums = new int[N];
-		res = Integer.MAX_VALUE;
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<N; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		comb(0,0);
-		System.out.println(answer);
-		
-	}
-	
-	static void comb(int start , int cnt) {
-		int k = Arrays.stream(ans).sum();
-		
-		if (cnt == 3) {
-			int tmp = Math.abs(M-k);
-			if (k <= M) {
-				if (tmp < res) {
-					res = tmp;
-					answer = k;
-				}
-			}
-			return;
-		}
-		
-		for (int i=start; i<N; i++) {
-			ans[cnt] = nums[i];
-			comb(i+1,cnt+1);
-		}
-	}
+  public static int N, M, ans;
+  public static int[] arr;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    arr = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    
+    ans = Integer.MIN_VALUE;
+    dfs(0, 0, 0);
+    System.out.println(ans);
+
+  }
+
+  public static void dfs(int start, int cnt, int sum) {
+    if (cnt == 3) {
+      if (sum == M) {
+        System.out.println(M);
+        System.exit(0);
+      }
+      if (sum <= M) {
+        ans = Math.max(ans, sum);
+      }
+      return;
+    }
+
+    for (int i = start; i < N; i++) {
+      dfs(i + 1, cnt + 1, sum + arr[i]);
+    }
+  }
 }

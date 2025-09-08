2b2f37cc31594019963dd7f7c3e0e2cdbe8af931
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-08 10:45:06 +0900

    [Bronze II] Title: 소수 찾기, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,31 +1,35 @@
-import java.io.*;
 import java.util.*;
- 
-class Main {
+import java.io.*;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
 
-		int  n = Integer.parseInt(br.readLine());
-		
-		int cnt = 0;
+    int n = Integer.parseInt(br.readLine());
 
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<n; i++) {
-			boolean flag = true;
-			int k = Integer.parseInt(st.nextToken());
-			if (k == 1) continue;
-			for (int j=2; j<k ;j++) {
-				if (k % j == 0) {
-					flag = false;
-					break;
-				}
+    int cnt = 0;
 
-			}
-			if (flag) cnt++;
-		}
-		System.out.println(cnt);
-	}
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      boolean yes = true;
+      int tmp = Integer.parseInt(st.nextToken());
+
+      if (tmp == 1)
+        yes = false;
+      for (int j = 2; j < tmp; j++) {
+        if (tmp % j == 0) {
+          yes = false;
+          break;
+        }
+      }
+      if (yes) {
+        cnt++;
+      }
+    }
+
+    System.out.println(cnt);
+
+  }
 }

af6d44118b6559499a9f81a502b67ff7786c7f08
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-09 17:40:18 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,38 +1,38 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	static int N, M;
-	static int[] selected;
-	static StringBuilder sb = new StringBuilder();
+public class Main {
+  public static int N, M;
+  public static int[] selected;
+  public static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
 
-		selected = new int[M];
+    selected = new int[M];
 
-		comb(0, 1);
-		System.out.println(sb);
-	}
+    perm(1, 0);
+    System.out.println(sb);
+  }
 
-	static void comb(int cnt, int start) {
-		if (cnt == M) {
-			for (int x : selected) {
-				sb.append(x).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
+  public static void perm(int start, int cnt) {
+    if (cnt == M) {
+      for (int i = 0; i < M; i++) {
+        sb.append(selected[i]).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
 
-		for (int i = start; i <= N; i++) {
+    for (int i = start; i <= N; i++) {
+      selected[cnt] = i;
+      perm(i + 1, cnt + 1);
 
-			selected[cnt] = i;
-			comb(cnt + 1, i + 1);
-		}
-	}
+    }
+  }
 }

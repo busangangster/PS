e03355a36c65ff7efe827f80c3891f2ecb006bc2
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-09 17:47:05 +0900

    [Silver III] Title: N과 M (5), Time: 304 ms, Memory: 28344 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,55 +1,52 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	static int N, M;
-	static int[] nums, selected;
-	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
+public class Main {
+  public static int N, M;
+  public static int[] arr;
+  public static int[] selected;
+  public static boolean[] visited;
+  public static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+    arr = new int[N];
+    selected = new int[M];
+    visited = new boolean[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
 
-		nums = new int[N];
+    Arrays.sort(arr);
+    comb(0);
+    System.out.println(sb);
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(nums);
+  }
 
-		selected = new int[M];
-		visited = new boolean[N + 1];
+  public static void comb(int cnt) {
+    if (cnt == M) {
+      for (int i = 0; i < M; i++) {
+        sb.append(selected[i]).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
 
-		perm(0);
-		System.out.println(sb);
-	}
+    for (int i = 0; i < N; i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        selected[cnt] = arr[i];
+        comb(cnt + 1);
+        visited[i] = false;
+      }
 
-	static void perm(int cnt) {
-		if (cnt == M) {
-			for (int x : selected) {
-				sb.append(x).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-
-		for (int i = 0; i < N; i++) {
-
-			if (visited[i])
-				continue;
-
-			selected[cnt] = nums[i];
-			visited[i] = true;
-			perm(cnt + 1);
-			visited[i] = false;
-		}
-	}
-
+    }
+  }
 }

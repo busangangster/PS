7c706d1ae9487b07eab4d4a6d75da142354f6635
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-13 15:31:13 +0900

    [Silver IV] Title: 보물, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,33 +1,35 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-        int[] a = new int[n];
-        Integer[] b = new Integer[n];
+    int n = Integer.parseInt(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
-        }
+    ArrayList<Integer> a, b;
+    a = new ArrayList<>();
+    b = new ArrayList<>();
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            b[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(a);
-        Arrays.sort(b, Collections.reverseOrder());
-        
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans += (a[i] * b[i]);
-        }
-        System.out.println(ans);
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      a.add(Integer.parseInt(st.nextToken()));
     }
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      b.add(Integer.parseInt(st.nextToken()));
+    }
+
+    Collections.sort(a);
+    Collections.sort(b, Collections.reverseOrder());
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+      ans += a.get(i) * b.get(i);
+    }
+    System.out.println(ans);
+  }
 }

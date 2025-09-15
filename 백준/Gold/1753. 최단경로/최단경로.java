a6f2f411faede01537d10baa20500fcdffc9adc8
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-15 13:48:20 +0900

    [Gold IV] Title: 최단경로, Time: 672 ms, Memory: 108484 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,86 +1,76 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    
-    static int V, E, K;
-    static int INF = Integer.MAX_VALUE;
-    static int[] min_dis;
-    static ArrayList<ArrayList<Node>> arr = new ArrayList<ArrayList<Node>>();
+  public static int V, E, INF;
+  public static ArrayList<ArrayList<Node>> arr = new ArrayList<ArrayList<Node>>();
+  public static int[] min_dis;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
 
-        st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    V = Integer.parseInt(st.nextToken());
+    E = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < V + 1; i++) {
-            arr.add(new ArrayList<>());
-        }
+    INF = 300000 * 10 + 1;
+    min_dis = new int[V + 1];
+    Arrays.fill(min_dis, INF);
 
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            arr.get(u).add(new Node(v, w));
-        }
+    int start = Integer.parseInt(br.readLine());
 
-        dijkstra(K);
-
-        for (int i = 1; i < V + 1; i++) {
-            sb.append(min_dis[i] == INF ? "INF" : min_dis[i]).append("\n");
-        }
-
-        System.out.println(sb);
-
+    for (int i = 0; i <= V; i++) {
+      arr.add(new ArrayList<>());
     }
-    
-    public static void dijkstra(int start) {
-        min_dis = new int[V + 1];
-        for (int i = 0; i < V + 1; i++) {
-            min_dis[i] = INF;
-        }
-        PriorityQueue<Node> pq = new PriorityQueue<Node>((o1, o2) -> o1.cost - o2.cost);
-        min_dis[start] = 0;
-        pq.offer(new Node(start, 0));
 
-        while (!pq.isEmpty()) {
-
-            Node cur = pq.poll();
-
-            if (min_dis[cur.idx] < cur.cost) {
-                continue;
-            }
-
-            for (int i = 0; i < arr.get(cur.idx).size(); i++) {
-                Node next_node = arr.get(cur.idx).get(i);
-                if (min_dis[next_node.idx] > cur.cost + next_node.cost) {
-                    min_dis[next_node.idx] = cur.cost + next_node.cost;
-                    pq.offer(new Node(next_node.idx, min_dis[next_node.idx]));
-
-                }
-
-            }
-        }
-    
+    for (int i = 0; i < E; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      int c = Integer.parseInt(st.nextToken());
+      arr.get(a).add(new Node(b, c));
     }
+
+    dijkstra(start);
+    for (int i = 1; i <= V; i++) {
+      if (min_dis[i] == INF) {
+        sb.append("INF");
+      } else
+        sb.append(min_dis[i]);
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static void dijkstra(int start) {
+    PriorityQueue<Node> pq = new PriorityQueue<Node>((o1, o2) -> o1.cost - o2.cost);
+    min_dis[start] = 0;
+    pq.offer(new Node(start, 0));
+
+    while (!pq.isEmpty()) {
+      Node cur = pq.poll();
+
+      if (min_dis[cur.node] < cur.cost)
+        continue;
+
+      for (Node next : arr.get(cur.node)) {
+        if (min_dis[next.node] > cur.cost + next.cost) {
+          min_dis[next.node] = cur.cost + next.cost;
+          pq.offer(new Node(next.node, cur.cost + next.cost));
+        }
+      }
+    }
+  }
 }
 
 class Node {
-    int idx;
-    int cost;
+  int node;
+  int cost;
 
-    public Node(int idx, int cost) {
-        this.idx = idx;
-        this.cost = cost;
-    }
-    @Override
-    public String toString() {
-        return "(" + idx + ", " + cost + ")";
-    }
+  public Node(int node, int cost) {
+    this.node = node;
+    this.cost = cost;
+  }
 }

b02b4c0ac1e0603b64a02342730c1cb2c1d47dbb
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-16 18:15:49 +0900

    [Silver IV] Title: 스택, Time: 164 ms, Memory: 18944 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,38 +1,43 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<>();
-		
-		for (int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			String cmd = st.nextToken();
-			if (cmd.equals("push")) {
-				int num = Integer.parseInt(st.nextToken());
-				stack.push(num);
-			}
-			else if (cmd.equals("pop")) {
-				if (stack.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(stack.pop()).append("\n");
-			}
-			else if (cmd.equals("size")) {
-				sb.append(stack.size()).append("\n");
-			}
-			else if (cmd.equals("empty")) {
-				if (stack.isEmpty()) sb.append(1).append("\n");
-				else sb.append(0).append("\n");
-			}
-			else if (cmd.equals("top")) {
-				if (stack.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(stack.peek()).append("\n");
-			}	
-		}
-		System.out.println(sb);	
-	}
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+    Stack<Integer> stack = new Stack<>();
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      String cmd = st.nextToken();
+      if (cmd.equals("push")) {
+        int k = Integer.parseInt(st.nextToken());
+        stack.push(k);
+      } else if (cmd.equals("top")) {
+        if (stack.isEmpty())
+          sb.append(-1);
+        else
+          sb.append(stack.peek());
+        sb.append("\n");
+      } else if (cmd.equals("size")) {
+        sb.append(stack.size());
+        sb.append("\n");
+      } else if (cmd.equals("empty")) {
+        if (stack.isEmpty())
+          sb.append(1);
+        else
+          sb.append(0);
+        sb.append("\n");
+      } else if (cmd.equals("pop")) {
+        if (stack.isEmpty())
+          sb.append(-1);
+        else
+          sb.append(stack.pop());
+        sb.append("\n");
+      }
+    }
+    System.out.println(sb);
+  }
 }

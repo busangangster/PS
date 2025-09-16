05a1ebe2c637647e370fd8ee6b0a13d2be70b8a5
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-16 18:09:49 +0900

    [Silver IV] Title: 수 찾기, Time: 524 ms, Memory: 49468 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,25 +1,29 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int N = Integer.parseInt(br.readLine());
-		HashMap<Integer,Integer> hm = new HashMap<>();
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<N;i++) {
-			hm.put(Integer.parseInt(st.nextToken()), 1);
-		}
-		
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for (int i=0; i<M; i++) {
-			if (hm.containsKey(Integer.parseInt(st.nextToken()))) sb.append(1).append("\n");
-			else sb.append(0).append("\n");
-		}
-		System.out.println(sb);
-	}
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+    HashMap<Integer, Integer> hm = new HashMap<>();
+    int n = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      hm.put(Integer.parseInt(st.nextToken()), 1);
+    }
+
+    int m = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < m; i++) {
+      if (hm.containsKey(Integer.parseInt(st.nextToken())))
+        sb.append(1);
+      else
+        sb.append(0);
+      sb.append("\n");
+    }
+    System.out.println(sb);
+
+  }
 }

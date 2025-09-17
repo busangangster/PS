4173a9af92fac94e3f6aa31373aa3024af50afdf
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-17 12:53:56 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 268 ms, Memory: 20604 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,44 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 
-		
-		while (true) {
-			Stack<Character> stack = new Stack<>();
-			String s = br.readLine();
-			if (s.equals(".")) break;
-			
-			for (int i=0; i<s.length();i++) {
-				char tmp = s.charAt(i);
-				if (tmp == '(' || tmp == '[') {
-					stack.add(tmp);
-				}
-				else if (tmp == ')') {
-					if (!stack.isEmpty() && stack.peek() == '(') {
-						stack.pop();
-					}
-					else stack.add(tmp);
-				}
-				else if (tmp == ']') {
-					if (!stack.isEmpty() && stack.peek() == '[') {
-						stack.pop();
-					}
-					else stack.add(tmp);
-				}
-				else continue;
-			}
-			
-			if (stack.isEmpty()) sb.append("yes");
-			else sb.append("no");
-			sb.append("\n");
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
 
-		}
-		System.out.println(sb);
-	}
-	
+    while (true) {
+      String s = br.readLine();
+      if (s.charAt(0) == '.') {
+        System.out.println(sb);
+        System.exit(0);
+      }
+
+      Stack<Character> stack = new Stack<>();
+
+      for (int i = 0; i < s.length(); i++) {
+        char tmp = s.charAt(i);
+        if (tmp == '(' || tmp == '[') {
+          stack.push(tmp);
+        } else if (tmp == ')') {
+          if (!stack.isEmpty() && stack.peek() == '(') {
+            stack.pop();
+          } else
+            stack.push(tmp);
+        } else if (tmp == ']') {
+          if (!stack.isEmpty() && stack.peek() == '[') {
+            stack.pop();
+          } else
+            stack.push(tmp);
+        }
+      }
+
+      if (!stack.isEmpty()) {
+        sb.append("no");
+      } else
+        sb.append("yes");
+      sb.append("\n");
+
+    }
+
+  }
 }

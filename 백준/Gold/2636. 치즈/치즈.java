b9dffb79aeffc9a30013a7d4c580ce6965412ccd
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-17 12:16:23 +0900

    [Gold IV] Title: 치즈, Time: 136 ms, Memory: 15204 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,107 +1,84 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-	static int N,M;
-	static int[][] graph;
-	static boolean[][] visited;
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {1,0,-1,0};
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		graph = new int[N][M];
+import java.io.*;
 
-		int time = 1;
-		int cheese = 0;
-		int before = 0;
-		
-		for (int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j=0; j<M; j++) {
-				graph[i][j] = Integer.parseInt(st.nextToken());
-				if (graph[i][j] == 1) {
-					cheese++;
-				}
-			}
-		}
-		
-		while (true) {
-			bfs();
-			melt();
-			before = cheese;
-			cheese = countUp();
-			if (cheese == 0) {
-				System.out.println(time);
-				System.out.println(before);
-				break;
-			}
-			time++;
-		}
-	}
-	
-	static void bfs() {
-		Queue<Node> q = new ArrayDeque<Node>();
-		visited = new boolean[N][M];
-		visited[0][0] = true;
-		q.offer(new Node(0,0));
-		
-		while (!q.isEmpty()) {
-			Node cur = q.poll();
-			
-			for (int i=0; i<4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				
-				if (!boundCheck(nx, ny)) continue;
-				if (visited[nx][ny]) continue;
-				
-				if (graph[nx][ny] == 0) {
-					q.offer(new Node(nx,ny));
-				}
-				else if (graph[nx][ny] == 1) {
-					graph[nx][ny] = -1;
-				}
-				visited[nx][ny]= true; 
-			}
-		}
-	}
-	
-	static void melt() {
-		for (int i=0; i<N; i++) {
-			for (int j=0; j<M; j++) {
-				if(graph[i][j] == -1) {
-					graph[i][j] = 0;
-				}
-			}
-		}
-	}
-	
-	static int countUp() {
-		int cnt = 0;
-		for (int i=0; i<N; i++) {
-			for (int j=0; j<M; j++) {
-				if (graph[i][j] == 1) cnt++;
-			}
-		}
-		return cnt;
-	}
-	
-	static boolean boundCheck(int x, int y) {
-		if (0 <= x && x < N && 0 <= y && y < M) return true;
-		else return false;
-	}
-	
-	static class Node{
-		int x,y;
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+public class Main {
+  public static int N, M, cheese;
+  public static int[][] arr;
+  public static boolean[][] visited;
+  public static int[] dx = { 0, 1, 0, -1 };
+  public static int[] dy = { 1, 0, -1, 0 };
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    cheese = 0;
+    int former = 0;
+    int cnt = 0;
+    arr = new int[N][M];
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < M; j++) {
+        int tmp = Integer.parseInt(st.nextToken());
+        if (tmp == 1)
+          cheese++;
+        arr[i][j] = tmp;
+      }
+    }
+
+    while (true) {
+      if (cheese == 0) {
+        System.out.println(cnt);
+        System.out.println(former);
+        break;
+      }
+      former = cheese;
+      bfs();
+      cnt++;
+    }
+  }
+
+  public static void bfs() {
+    visited = new boolean[N][M];
+    Queue<int[]> q = new ArrayDeque<>();
+    q.offer(new int[] { 0, 0 });
+    visited[0][0] = true;
+
+    while (!q.isEmpty()) {
+      int[] cur = q.poll();
+
+      for (int i = 0; i < 4; i++) {
+        int nx = cur[0] + dx[i];
+        int ny = cur[1] + dy[i];
+
+        if (check(nx, ny)) {
+          if (!visited[nx][ny]) {
+            if (arr[nx][ny] == 1) {
+              visited[nx][ny] = true;
+            } else {
+              visited[nx][ny] = true;
+              q.offer(new int[] { nx, ny });
+            }
+          }
+        }
+      }
+    }
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        if (arr[i][j] == 1 && visited[i][j]) {
+          arr[i][j] = 0;
+          cheese--;
+        }
+      }
+    }
+  }
+
+  public static boolean check(int x, int y) {
+    return (0 <= x && x < N && 0 <= y && y < M);
+  }
 }

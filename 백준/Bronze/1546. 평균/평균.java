0f9ec1856f654dfd19de691f0de082ba2a35da32
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-18 19:10:36 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,29 +1,29 @@
-import java.io.*;
 import java.util.*;
- 
-class Main {
+import java.io.*;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+public class Main {
 
-		int n = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		double[] arr = new double[n];
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
 
-		double max_v = Arrays.stream(arr).max().getAsDouble();
-		
-		for (int i=0; i<n; i++) {
-			arr[i] = arr[i]/max_v *100;
-		}
+    int n = Integer.parseInt(br.readLine());
+    int[] score = new int[n];
+    int max = Integer.MIN_VALUE;
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      int tmp = Integer.parseInt(st.nextToken());
+      score[i] = tmp;
+      max = Math.max(max, tmp);
+    }
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+      double k = (double) score[i] / (double) max * 100;
+      sum += k;
+    }
 
-		double sum = Arrays.stream(arr).sum();
-		
-		System.out.println(sum/arr.length);
-		
-	}
+    System.out.println(sum / n);
+
+  }
 }

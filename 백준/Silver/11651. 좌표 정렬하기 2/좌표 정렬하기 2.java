18591a7ad3724939f1274fa2fa1543c85daf656b
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-22 17:08:44 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 720 ms, Memory: 58240 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -2,41 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		ArrayList<Dot> arr = new ArrayList<>();
-		
-		for (int i=0; i<n;i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			arr.add(new Dot(x,y));
-		}
-		
-		Collections.sort(arr);
-		
-		for (Dot cur: arr) {
-			sb.append(cur.x).append(" ").append(cur.y).append("\n");
-		}
-		System.out.println(sb);
-	}
-	static class Dot implements Comparable<Dot>{
-		int x,y;
-		public Dot(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-		@Override
-		public int compareTo(Dot o) {
-			if (this.y == o.y) {
-				return this.x - o.x;
-			}
-			return this.y - o.y;
-		}
-	}
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+    int n = Integer.parseInt(br.readLine());
+
+    ArrayList<Node> arr = new ArrayList<>();
+
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+      arr.add(new Node(x, y));
+    }
+
+    Collections.sort(arr);
+
+    for (Node tmp : arr) {
+      sb.append(tmp.x + " " + tmp.y).append("\n");
+    }
+    System.out.println(sb);
+  }
+}
+
+class Node implements Comparable<Node> {
+  int x;
+  int y;
+
+  public Node(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  @Override
+  public int compareTo(Node o) {
+    if (this.y == o.y) {
+      return this.x - o.x;
+    }
+    return this.y - o.y;
+  }
 }

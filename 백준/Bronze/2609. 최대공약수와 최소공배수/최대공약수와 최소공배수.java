1112a1b2b8d125f27e67ff77f0d40b031fd41501
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-22 17:12:28 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -2,28 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		
-		int tmp = gcd(a,b);
-		int lcm = a*b/tmp;
-		
-		sb.append(tmp).append("\n");
-		sb.append(lcm);
-		
-		System.out.println(sb);
-		
-	}
-	
-	static int gcd(int a, int b) {
-		if (b == 0) return a;
-		
-		return gcd(b,a%b);
-	}
+  public static int a, b;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    a = Integer.parseInt(st.nextToken());
+    b = Integer.parseInt(st.nextToken());
+
+    int res = gcd(a, b);
+    System.out.println(res);
+    System.out.println(a * b / res);
+  }
+
+  public static int gcd(int x, int y) {
+    if (y == 0)
+      return x;
+    else
+      return gcd(y, x % y);
+  }
 }

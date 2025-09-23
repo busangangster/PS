1dfe278b7bc61b574973593597993fb190160cef
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-23 13:26:58 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 628 ms, Memory: 48828 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,95 +1,86 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main {
-	
-	static int N,M,target_x,target_y;
-	static boolean[][] visited;
-	static int[][] graph;
-	static int[][] ans;
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {1,0,-1,0};
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		graph = new int[N][M];
-		ans = new int[N][M];
-		
-		for (int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j=0; j<M; j++) {
-				graph[i][j] = Integer.parseInt(st.nextToken());
-				if (graph[i][j] == 2) {
-					target_x = i;
-					target_y = j;
-				}
-			}
-		}
-		
-		for (int i=0;i <N; i++) {
-			for (int j= 0; j<M; j++) {
-				if (graph[i][j] == 1) {
-					graph[i][j] = -1;
-				}
-			}
-		}
-		bfs();
-		for (int[] x : graph) {
-			for (int val : x) {
-				sb.append(val).append(" ");
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb);
-		
-	}
-	
-	static void bfs() {
-		Queue<Node> q = new ArrayDeque<Node>();
-		visited = new boolean[N][M];
-		visited[target_x][target_y] = true;
-		q.offer(new Node(target_x,target_y,0));
-		graph[target_x][target_y] = 0;
-		
-		while (!q.isEmpty()) {
-			Node cur = q.poll();
-			
-			for (int i=0; i<4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				
-				if (!check(nx,ny)) continue;
-				if (visited[nx][ny]) continue;
-				
-				if (graph[nx][ny] == -1) {
-					visited[nx][ny] = true;
-					graph[nx][ny] = cur.dis +1;
-					q.offer(new Node(nx,ny,cur.dis+1));
-				}
-				else if (graph[nx][ny] == 0) {
-					visited[nx][ny] = true;
-				}
- 			}
-		}
-	}
-	
-	static boolean check(int x, int y) {
-		if (0 <= x && x < N && 0 <= y && y < M) return true;
-		else return false;
-	}
-	
-	static class Node{
-		int x,y,dis;
-		public Node(int x, int y, int dis) {
-			this.x = x;
-			this.y = y;
-			this.dis = dis;
-		}
-	}
+  public static int N, M, tx, ty;
+  public static int[][] arr;
+  public static int[][] ans;
+  public static boolean[][] visited;
+  public static int[] dx = { 0, 1, 0, -1 };
+  public static int[] dy = { 1, 0, -1, 0 };
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    arr = new int[N][M];
+    ans = new int[N][M];
+    visited = new boolean[N][M];
+
+    for (int[] a : ans) {
+      Arrays.fill(a, -1);
+    }
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < M; j++) {
+        int tmp = Integer.parseInt(st.nextToken());
+        if (tmp == 2) {
+          tx = i;
+          ty = j;
+        } else if (tmp == 0) {
+          ans[i][j] = 0;
+        }
+        arr[i][j] = tmp;
+      }
+    }
+
+    bfs(tx, ty);
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        sb.append(ans[i][j]).append(" ");
+      }
+      sb.append("\n");
+    }
+
+    System.out.println(sb);
+
+  }
+
+  public static void bfs(int x, int y) {
+    Queue<int[]> q = new ArrayDeque<>();
+    visited[x][y] = true;
+    ans[x][y] = 0;
+    q.offer(new int[] { x, y, 0 });
+
+    while (!q.isEmpty()) {
+      int[] cur = q.poll();
+
+      for (int i = 0; i < 4; i++) {
+        int nx = cur[0] + dx[i];
+        int ny = cur[1] + dy[i];
+
+        if (check(nx, ny)) {
+          if (!visited[nx][ny]) {
+            if (arr[nx][ny] == 0) {
+              visited[nx][ny] = true;
+            } else {
+              ans[nx][ny] = cur[2] + 1;
+              visited[nx][ny] = true;
+              q.offer(new int[] { nx, ny, cur[2] + 1 });
+            }
+          }
+        }
+      }
+    }
+  }
+
+  public static boolean check(int x, int y) {
+    return (0 <= x && x < N && 0 <= y && y < M);
+  }
 }

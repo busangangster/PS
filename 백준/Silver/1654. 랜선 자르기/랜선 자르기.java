41f071d55d9cbecef5aa236e39794addc67c155c
Busangangster <83652134+busangangster@users.noreply.github.com>
2025-09-23 14:23:18 +0900

    [Silver II] Title: 랜선 자르기, Time: 180 ms, Memory: 17388 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,55 +1,49 @@
-import java.io.*;
 import java.util.*;
-
-/*
-CheckPoints !!
-
-- 이분탐색문제인 건 빠르게 알았지만, 자료형을 잘 체크했어야 함.
-lt가 1이고, rt는 최댓값인 2^31-1이 들어가는데, 이 둘을 더할 경우 int의 범위를 벗어나게 됨.
-떄문에 long형으로 선언해줬어야 함 !! 
-*/
+import java.io.*;
 
 public class Main {
-	static int K,N,max_v;
-	static int[] arr;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		K = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		
-		arr = new int[K];
-		for (int i=0; i<K; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		max_v = Arrays.stream(arr).max().getAsInt();
-		
-		long lt = 1;
-		long rt = max_v;
-		long ans = 0;
-		
-		while (lt <= rt) {
-			long mid = (lt+rt) /2 ;
-			if (checkCnt(mid) >= N) {
-				ans = mid;
-				lt = mid+1 ;
-			}
-			else {
-				rt = mid-1;
-			}
-		}
-		System.out.println(ans);
-	
-	}
-	
-	static long checkCnt(long target) {
-		long cnt = 0;
-		for (int i=0; i<K;i++) {
-			cnt += (arr[i] / target);
-		}
-		return cnt;
-	}
+  public static int K, N;
+  public static long ans;
+  public static int[] lines;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    K = Integer.parseInt(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
+
+    lines = new int[K];
+    for (int i = 0; i < K; i++) {
+      lines[i] = Integer.parseInt(br.readLine());
+    }
+    Arrays.sort(lines);
+    bs();
+    System.out.println(ans);
+  }
+
+  public static void bs() {
+    long lt = 1L;
+    long rt = lines[K - 1];
+
+    while (lt <= rt) {
+      long mid = lt + (rt - lt) / 2;
+      if (check(mid)) {
+        ans = mid;
+        lt = mid + 1;
+      } else
+        rt = mid - 1;
+    }
+  }
+
+  public static boolean check(long target) {
+    long res = 0L;
+    for (int tmp : lines) {
+      res += tmp / target;
+      if (res >= N)
+        return true;
+    }
+    return false;
+  }
 }
